b1c679721d13e18a2b29e053e82f90fba0860aec
Liyan Zheng <liyan-zheng@outlook.com>
2022-09-03 13:35:47 +0800

    Fix: change the path of logs
    ```
    85% tests passed, 4 tests failed out of 27
    
    Total Test time (real) = 100.69 sec
    
    The following tests FAILED:
             10 - test_conv2conv (Timeout)
             11 - test_conv2gemm (Timeout)
             15 - test_guidedDLT (Subprocess aborted)
             21 - test_mergeStage (Subprocess aborted)
    Errors while running CTest
    ```
    
    - test_conv2conv 38529 ms total
    - test_conv2gemm 37098 ms total

--- test/nnet/test_TConv2gemm.cc
+++ test/nnet/test_TConv2gemm.cc
@@ -364,14 +364,14 @@ TEST(TConv2gemm, TConv4x4_NHWF_RSFC_search) {
     //     dbg(CountRoutineVisitor().count(f.root));
     // }
     int nMatches = matchExprResult(
-        derivator, "../test/log/TConv4x4_NHWF_RSFC/TConv4x4_NHWF_RSFC_18.expr");
+        derivator, "../test/nnet/log/TConv4x4_NHWF_RSFC/TConv4x4_NHWF_RSFC_18.expr");
     EXPECT_GE(nMatches, 1);
     derivator.printStatistics();
 }
 
 TEST(TConv2gemm, TConv4x4_NHWF_FRSC_CheckDerivationCorrectness_log) {
     const string fnPrefix =
-        "../test/log/TConv4x4_NHWF_RSFC/TConv4x4_NHWF_RSFC_";
+        "../test/nnet/log/TConv4x4_NHWF_RSFC/TConv4x4_NHWF_RSFC_";
     EXPECT_TRUE(checkExprLogSame(fnPrefix, 0, 11));
 }
 
