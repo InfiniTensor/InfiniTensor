1c8a5f55a01800c41205fb4cb2ee8262de6f7421
wanghailu <wanghailu@qiyuanlab.com>
2022-09-07 14:39:35 +0800

    add protobuf

--- test/core/test_proto.cc
+++ test/core/test_proto.cc
@@ -1,14 +1,21 @@
 #include "test.h"
 #include <string>
 #include <iostream>
-#include "tensor.pb.h"
+#include <vector>
+#include "data.pb.h"
 
 namespace infini {
 
 TEST(Protobuf, easy_test) {
     std::string test = "an unhappy tensor";
-    tensor::tensor222 my_tensor;
-    my_tensor.set_name("a happy tensor");
+    std::vector<int> a {1,2,2,3};
+    std::vector<float> data {1.2, 2.3, 3.4, 4.5, 5.6, 6.7, 7.8, 8.9, 9.1, 10.1, 11.2, 12.3};
+    data::Tensor my_tensor;
+    my_tensor.set_id("a happy tensor");
+    my_tensor.mutable_shape()->CopyFrom({a.begin(), a.end()});
+    my_tensor.set_layout(data::LAYOUT_NHWC);
+    my_tensor.set_dtype(data::DTYPE_FLOAT);
+    my_tensor.mutable_data_float()->CopyFrom({data.begin(), data.end()});
     my_tensor.SerializeToString(&test);
     std::cout << test << std::endl;
 }
