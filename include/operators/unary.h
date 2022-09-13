e252ce7dc65d5f5611dd675c52de3b187304aeeb
wanghailu <wanghailu@qiyuanlab.com>
2022-09-13 16:20:40 +0800

    add code for activation operation

---
+++ include/operators/unary.h
@@ -0,0 +1,31 @@
+#pragma once
+#include "core/operator.h"
+
+namespace infini {
+class UnaryObj : public OperatorObj {
+  public:
+    UnaryObj(OpType type, GraphObj *graph, Tensor input, Tensor output);
+    optional<vector<Shape>> inferShape(const TensorVec &inputs) const override;
+
+    std::string toString() const override;
+    int numInputs() const override { return 1; }
+    int numOutputs() const override { return 1; }
+
+  private:
+    vector<int> getWorkloadVector() const override;
+    vector<int> getOpAttrVector() const override;
+};
+
+#define DEFINE_UNARY_OBJ(prefix, type)                                         \
+    class prefix##Obj : public UnaryObj {                                      \
+      public:                                                                  \
+        prefix##Obj(GraphObj *graph, Tensor input, Tensor output)              \
+            : UnaryObj(type, graph, input, output) {}                          \
+    };
+
+DEFINE_UNARY_OBJ(Relu, OpType::Relu)
+DEFINE_UNARY_OBJ(Sigmoid, OpType::Sigmoid)
+DEFINE_UNARY_OBJ(Tanh, OpType::Tanh)
+DEFINE_UNARY_OBJ(Softmax, OpType::Softmax)
+DEFINE_UNARY_OBJ(Abs, OpType::Abs)
+}; // namespace infini
---
+++ src/operators/unary.cc
@@ -0,0 +1,36 @@
+#include "operators/unary.h"
+
+namespace infini {
+UnaryObj::UnaryObj(OpType type, GraphObj *graph, Tensor input, Tensor output)
+    : OperatorObj(type, {input}, {output}) {
+    IT_ASSERT(checkValid(graph));
+}
+
+optional<vector<Shape>>
+UnaryObj::inferShape(const TensorVec &inputs) const {
+    const auto A = inputs[0];
+    return {{A->getDims()}};
+}
+
+std::string UnaryObj::toString() const {
+    std::ostringstream os;
+    os << OpRegistry::getOpName(type) << "[" << getGuid() << "]";
+    os << "(";
+    os << vecToString(inputs[0]->getDims()) << ",";
+    os << "input=" << inputs[0]->getGuid() << ",";
+    os << "output=" << outputs[0]->getGuid() << ")";
+    return os.str();
+}
+
+// use output dim or inputs dim?
+vector<int> UnaryObj::getWorkloadVector() const {
+    vector<int> ret = outputs[0]->getDims();
+    ret.emplace(ret.begin(), enum_to_underlying(type));
+    return ret;
+}
+
+vector<int> UnaryObj::getOpAttrVector() const {
+    return {enum_to_underlying(type)};
+}
+
+}; // namespace infini
