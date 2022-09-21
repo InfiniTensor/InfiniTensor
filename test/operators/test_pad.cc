520b155f0c22e4d35d660a6112d227309645d1ab
wangw <271502003@qq.com>
2022-09-21 15:07:00 +0800

    ADD:pad/slice operator and cuda kernel.
    fix compile error
    
    refector
    
    clang format
    
    split test.
    
    fix compile error.
    
    ADD slice cuda kernel.
    
    ADD slice operator.
    
    ADD:pad operator and cuda kernel.

---
+++ test/operators/test_pad.cc
@@ -0,0 +1,25 @@
+#include "core/graph.h"
+#include "core/runtime.h"
+#include "operators/pad.h"
+#include "test.h"
+
+namespace infini {
+TEST(Pad, ShapeInference) {
+    Runtime cpuRuntime = CpuRuntimeObj::getInstance();
+    {
+        Graph g = make_ref<GraphObj>(cpuRuntime);
+        Tensor i = g->addTensor({1, 64, 162, 162}, DataType::UInt32);
+        auto op = g->addOp<PadObj>(
+            i, nullptr, vector<int>{2, 10, 1, 5, 0, 10, 1, 5}, std::nullopt);
+        EXPECT_EQ(op->getOutput()->getDims(), (Shape{3, 84, 164, 172}));
+    }
+    {
+        Graph g = make_ref<GraphObj>(cpuRuntime);
+        Tensor i = g->addTensor({1, 64, 162, 162}, DataType::UInt32);
+        auto op = g->addOp<PadObj>(i, nullptr, vector<int>{2, 10, 1, 5},
+                                   vector<int>{0, 3});
+        EXPECT_EQ(op->getOutput()->getDims(), (Shape{4, 64, 162, 177}));
+    }
+}
+
+} // namespace infini
