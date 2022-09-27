1aefc1b27ef7b846768877b7106227e71e96363b
zhengly123 <zhengly123@outlook.com>
2022-09-27 10:41:12 +0800

    Add python interface for CUDA operator evaluation (#42)
    * Refactor: seperate data generator
    
    * Add: python bindings for opTimer
    
    * Fix: test_perfengine
    
    Co-authored-by: Liyan Zheng <liyan-zheng@outlook.com>

---
+++ include/cuda/operator_timer.h
@@ -0,0 +1,11 @@
+#pragma once
+namespace infini {
+namespace opTimer {
+double getPerfConvCudnn(int n, int c, int h, int w, int f, int r, int s,
+                        int padh, int padw, int strideh, int stridew,
+                        int dilationh, int dilationw, int group,
+                        const char *name);
+
+double getPerfMatmulCublas(int b, int m, int n, int k, const char *name);
+} // namespace opTimer
+} // namespace infini
--- test/kernels/cuda/test_perfengine.cc
+++ test/kernels/cuda/test_perfengine.cc
@@ -14,36 +14,27 @@ TEST(PerfEngine, save_and_load) {
     Runtime cpu = CpuRuntimeObj::getInstance(); // CPUruntime is singleton
     Graph gCpu = make_ref<GraphObj>(cpu);
     Runtime cuda = make_ref<CudaRuntimeObj>();
-    Graph gCuda = make_ref<GraphObj>(cuda);
-    // Set input data on CPU in a CPU Graph
-    Tensor i0Cpu = gCpu->addTensor({1, 3, 224, 224}, DataType::Float32);
-    Tensor w0Cpu = gCpu->addTensor({2, 3, 3, 3}, DataType::Float32);
-    // Malloc data for all tensors in a graph. Do we need implicit allocation?
-    gCpu->dataMalloc();
-    i0Cpu->setData(IncrementalGenerator());
-    w0Cpu->setData(IncrementalGenerator());
+    { // Conv
+        Graph gCuda = make_ref<GraphObj>(cuda);
 
-    // Copy input tensors from CPU to CUDA
-    Tensor i0Cuda = gCuda->cloneTensor(i0Cpu);
-    Tensor w0Cuda = gCuda->cloneTensor(w0Cpu);
-    // Build CUDA graph
-    auto conv =
-        gCuda->addOp<ConvObj>(i0Cuda, w0Cuda, nullptr, 1, 1, 1, 1, 1, 1);
+        // Copy input tensors from CPU to CUDA
+        Tensor i0Cuda = gCuda->addTensor({1, 3, 224, 224}, DataType::Float32);
+        Tensor w0Cuda = gCuda->addTensor({2, 3, 3, 3}, DataType::Float32);
+        // Build CUDA graph
+        auto conv =
+            gCuda->addOp<ConvObj>(i0Cuda, w0Cuda, nullptr, 1, 1, 1, 1, 1, 1);
+        gCuda->dataMalloc();
+        cuda->run(gCuda, true);
+    }
 
-    auto ACpu = gCpu->addTensor(Shape{1, 3, 5}, DataType::Float32);
-    auto BCpu = gCpu->addTensor(Shape{1, 5, 2}, DataType::Float32);
-    gCpu->dataMalloc();
-    ACpu->setData(IncrementalGenerator());
-    BCpu->setData(IncrementalGenerator());
-
-    auto cudaRuntime = make_ref<CudaRuntimeObj>();
-
-    auto ACuda = gCuda->cloneTensor(ACpu);
-    auto BCuda = gCuda->cloneTensor(BCpu);
-    auto matmul = gCuda->addOp<MatmulObj>(ACuda, BCuda, nullptr);
-
-    gCuda->dataMalloc();
-    cudaRuntime->run(gCuda, true);
+    { // Matmul
+        Graph gCuda = make_ref<GraphObj>(cuda);
+        auto ACuda = gCuda->addTensor(Shape{1, 3, 5}, DataType::Float32);
+        auto BCuda = gCuda->addTensor(Shape{1, 5, 2}, DataType::Float32);
+        auto matmul = gCuda->addOp<MatmulObj>(ACuda, BCuda, nullptr);
+        gCuda->dataMalloc();
+        cuda->run(gCuda, true);
+    }
     auto &perfEngine = PerfEngine::getInstance();
 
     json j0 = perfEngine;
