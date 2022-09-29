26cee55e817ef7880e6482af3573f544b59498e0
wendy12022 <fay-wangwen@163.com>
2022-09-29 14:52:50 +0800

    ADD:extend operator and cuda kernel. (#40)
    Co-authored-by: Haojie Wang <haojie0429@gmail.com>

---
+++ test/operators/test_extend.cc
@@ -0,0 +1,20 @@
+#include "core/graph.h"
+#include "core/kernel.h"
+#include "core/runtime.h"
+#include "operators/extend.h"
+
+#include "test.h"
+
+namespace infini {
+
+TEST(Extend, ShapeInference) {
+    Runtime runtime = CpuRuntimeObj::getInstance();
+    {
+        Graph g = make_ref<GraphObj>(runtime);
+        Tensor i = g->addTensor({2, 3, 3, 4}, DataType::Float32);
+        auto op = g->addOp<ExtendObj>(i, nullptr, 2, 1);
+        EXPECT_EQ(op->getOutput()->getDims(), (Shape{2, 3, 6, 4}));
+    }
+}
+
+} // namespace infini
