1a91f355816c6717b191442a5879722002b05235
Liyan Zheng <liyan-zheng@outlook.com>
2022-10-04 14:22:21 +0800

    Add: python interfaced for timing operators

--- python/infinitensor/operator_timer.py
+++ python/infinitensor/operator_timer.py
@@ -2,12 +2,14 @@ from tokenize import Double
 import pyinfinitensor  # import getPerfConv, getPerfMatmul
 
 
-def getPerfConv(n, c, h, w, f, r, s, padh, padw, strideh, stridew, dilationh, dilationw, group, name):
+def getPerfConv(n, c, h, w, f, r, s, padh, padw, strideh, stridew, dilationh, dilationw, group, name=""):
     return pyinfinitensor.getPerfConvCudnn(n, c, h, w, f, r, s, padh, padw,
-                               strideh, stridew, dilationh, dilationw, group, name)
+                                           strideh, stridew, dilationh, dilationw, group, name)
 
 
-def getPerfMatmul(b, m, n, k, name):
+def getPerfConvTransposed2dCudnn(n, c, h, w, f, r, s, padh, padw, strideh, stridew, dilationh, dilationw, oph, opw, group):
+    return pyinfinitensor.getPerfConvTransposed2dCudnn(n, c, h, w, f, r, s, padh, padw, strideh, stridew, dilationh, dilationw, oph, opw, group)
+
+
+def getPerfMatmul(b, m, n, k, name=""):
     return pyinfinitensor.getPerfMatmulCublas(b, m, n, k, name)
-
-
