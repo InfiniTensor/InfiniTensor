67c06733e65886d3ae74859bb0994d8af92932e0
Liyan Zheng <liyan-zheng@outlook.com>
2022-10-22 13:24:42 +0800

    Chore: format

--- src/kernels/cuda/conv2dreduce.cu
+++ src/kernels/cuda/conv2dreduce.cu
@@ -1,13 +1,12 @@
 #include "cuda/cuda_common.h"
 
-__global__ void conv2dreduce_kernel_(
-    float *__restrict__ input,
-    float *__restrict__ bias,
-    float *__restrict__ output,
-    const bool PReLU, const float paramReLU, const int n, const int f, const int h, const int w, 
-    const int oh, const int ow, const int r, const int s, const int ph,
-    const int pw, const int dh, const int dw, const int sh, const int sw)
-{
+__global__ void
+conv2dreduce_kernel_(float *__restrict__ input, float *__restrict__ bias,
+                     float *__restrict__ output, const bool PReLU,
+                     const float paramReLU, const int n, const int f,
+                     const int h, const int w, const int oh, const int ow,
+                     const int r, const int s, const int ph, const int pw,
+                     const int dh, const int dw, const int sh, const int sw) {
     // output shape: (n, oh, ow, f)
     // input shape: (n, h, w, f, r, s)
     int nid = blockIdx.x, fid = blockIdx.y;
@@ -23,7 +22,8 @@ __global__ void conv2dreduce_kernel_(
                 int ihid = ihst + ri * dh;
                 int iwid = iwst + si * dw;
                 if (ihid >= 0 && ihid < h && iwid >= 0 && iwid < w) {
-                    imm += *(nfinput + ihid * hchunk + iwid * wchunk + ri * s + si);
+                    imm += *(nfinput + ihid * hchunk + iwid * wchunk + ri * s +
+                             si);
                 }
             }
         }
@@ -38,17 +38,16 @@ __global__ void conv2dreduce_kernel_(
 }
 
 __global__ void convTranspose2dreduce_kernel_(
-    float *__restrict__ input,
-    float *__restrict__ bias,
-    float *__restrict__ output,
-    const bool PReLU, const float paramReLU, const int n, const int f, const int h, const int w, 
-    const int oh, const int ow, const int r, const int s, const int ph,
-    const int pw, const int dh, const int dw, const int sh, const int sw)
-{
+    float *__restrict__ input, float *__restrict__ bias,
+    float *__restrict__ output, const bool PReLU, const float paramReLU,
+    const int n, const int f, const int h, const int w, const int oh,
+    const int ow, const int r, const int s, const int ph, const int pw,
+    const int dh, const int dw, const int sh, const int sw) {
     // assert dh = dw = 1
     int nid = blockIdx.x, fid = blockIdx.y;
     int hid = threadIdx.x, wid = threadIdx.y;
-    const int fchunck = r * s, wchunk = f * fchunck, hchunk = w * wchunk, nchunck = n * hchunk;
+    const int fchunck = r * s, wchunk = f * fchunck, hchunk = w * wchunk,
+              nchunck = n * hchunk;
     float *nfinput = input + nid * nchunck + fid * fchunck;
     // view as conv, the true ph and pw
     int tph = r - ph - 1, tpw = s - pw - 1;
@@ -61,11 +60,10 @@ __global__ void convTranspose2dreduce_kernel_(
             for (int si = 0; si < s; ++si) {
                 int ihid = ihst + r - ri - 1;
                 int iwid = iwst + s - si - 1;
-                if  (ihid >= 0 && ihid < th &&
-                    iwid >= 0 && iwid < tw &&
-                    (ihid % sh == 0) && (iwid % sw == 0))
-                {
-                    imm += *(nfinput + (ihid / sh) * hchunk + (iwid / sw) * wchunk + ri * s + si);
+                if (ihid >= 0 && ihid < th && iwid >= 0 && iwid < tw &&
+                    (ihid % sh == 0) && (iwid % sw == 0)) {
+                    imm += *(nfinput + (ihid / sh) * hchunk +
+                             (iwid / sw) * wchunk + ri * s + si);
                 }
             }
         }
@@ -81,11 +79,10 @@ __global__ void convTranspose2dreduce_kernel_(
 
 namespace infini {
 
-void conv2dreduce_kernel(float *input, float *bias, float *output, 
-                          bool PReLU, float paramReLU, int n, int h, int w, int f,
-                          int r, int s, int oh, int ow, int ph, int pw, int sh,
-                          int sw, int dh, int dw)
-{
+void conv2dreduce_kernel(float *input, float *bias, float *output, bool PReLU,
+                         float paramReLU, int n, int h, int w, int f, int r,
+                         int s, int oh, int ow, int ph, int pw, int sh, int sw,
+                         int dh, int dw) {
     dim3 grid(n, f);
     dim3 block(oh, ow);
     cudaStream_t stream(cudaStreamPerThread);
@@ -93,15 +90,16 @@ void conv2dreduce_kernel(float *input, float *bias, float *output,
                                         oh, ow, r, s, ph, pw, dh, dw, sh, sw);
 }
 
-void convTranspose2dreduce_kernel(float *input, float *bias, float *output, 
-                          bool PReLU, float paramReLU, int n, int h, int w, int f,
-                          int r, int s, int oh, int ow, int ph, int pw, int sh,
-                          int sw, int dh, int dw)
-{
+void convTranspose2dreduce_kernel(float *input, float *bias, float *output,
+                                  bool PReLU, float paramReLU, int n, int h,
+                                  int w, int f, int r, int s, int oh, int ow,
+                                  int ph, int pw, int sh, int sw, int dh,
+                                  int dw) {
     dim3 grid(n, f);
     dim3 block(oh, ow);
     cudaStream_t stream(cudaStreamPerThread);
-    convTranspose2dreduce_kernel_<<<grid, block, 0,stream>>>(input, bias, output, PReLU, paramReLU, n, f, h, w, 
-                                        oh, ow, r, s, ph, pw, dh, dw, sh, sw);
+    convTranspose2dreduce_kernel_<<<grid, block, 0, stream>>>(
+        input, bias, output, PReLU, paramReLU, n, f, h, w, oh, ow, r, s, ph, pw,
+        dh, dw, sh, sw);
 }
-}
+} // namespace infini
