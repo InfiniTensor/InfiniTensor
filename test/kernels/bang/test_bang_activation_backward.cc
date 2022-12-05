769d80b922da8671b7851b02f55af5302d27fb9f
wanghailu <wanghailu0717@163.com>
2022-12-05 08:45:39 +0000

    add activation backward operation

---
+++ test/kernels/bang/test_bang_activation_backward.cc
@@ -0,0 +1,52 @@
+#include "bang/bang_runtime.h"
+#include "core/graph.h"
+#include "core/kernel.h"
+#include "core/runtime.h"
+#include "operators/activation_backward.h"
+
+#include "test.h"
+
+namespace infini {
+
+template <class T>
+void testActivationBackward(const std::function<void(void *, size_t, DataType)> &generator,
+               const Shape &shape) {
+    // Runtime
+    Runtime cpuRuntime = CpuRuntimeObj::getInstance();
+    auto bangRuntime = make_ref<BangRuntimeObj>();
+
+    // Build input data on CPU
+    Tensor yCpu = make_ref<TensorObj>(shape, DataType::Float32, cpuRuntime);
+    Tensor diffYCpu = make_ref<TensorObj>(shape, DataType::Float32, cpuRuntime);
+    Tensor xCpu = make_ref<TensorObj>(shape, DataType::Float32, cpuRuntime);
+
+    yCpu->dataMalloc();
+    diffYCpu->dataMalloc();
+    xCpu->dataMalloc();
+
+    yCpu->setData(generator);
+    diffYCpu->setData(generator);
+    xCpu->setData(generator);
+
+    // GPU
+    Graph bangGraph = make_ref<GraphObj>(bangRuntime);
+    auto yGpu = bangGraph->cloneTensor(yCpu);
+    auto diffYGpu = bangGraph->cloneTensor(diffYCpu);
+    auto xGpu = bangGraph->cloneTensor(xCpu);
+    auto gpuOp = bangGraph->addOp<T>(yGpu, diffYGpu, xGpu, nullptr);
+    bangGraph->dataMalloc();
+    bangRuntime->run(bangGraph);
+    auto diffXGpu = gpuOp->getOutput();
+    auto diffXGpu2Cpu = diffXGpu->clone(cpuRuntime);
+    // Check
+    diffXGpu2Cpu->print();
+    EXPECT_TRUE(1);
+}
+
+TEST(cnnl_ActivationBackward, run) {
+    testActivationBackward<ReluBackwardObj>(IncrementalGenerator(), Shape{1, 2, 2, 3});
+    testActivationBackward<SigmoidBackwardObj>(IncrementalGenerator(), Shape{1, 2, 2, 3});
+    testActivationBackward<TanhBackwardObj>(IncrementalGenerator(), Shape{1, 2, 2, 3});
+}
+
+} // namespace infini
