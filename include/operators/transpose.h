212a443e32723484358913b7a55643c818e1fab1
wanghailu <wanghailu0717@163.com>
2022-12-12 05:17:53 +0000

    add transpsoe code and test

---
+++ include/operators/transpose.h
@@ -0,0 +1,20 @@
+#pragma once
+#include "core/operator.h"
+
+namespace infini {
+class TransposeObj : public OperatorObj {
+  public:
+    TransposeObj(GraphObj *graph, Tensor input, Tensor output, int permute[4]);
+    optional<vector<Shape>> inferShape(const TensorVec &inputs) const override;
+
+    std::string toString() const override;
+    int numInputs() const override { return 1; }
+    int numOutputs() const override { return 1; }
+    auto getPermute() { return transposePermute; }
+
+  private:
+    int transposePermute[4];
+    vector<int> getWorkloadVector() const override;
+    vector<int> getOpAttrVector() const override;
+};
+}; // namespace infini
