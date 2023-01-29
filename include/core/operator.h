024f627519bc7380fcb96e9094b1b855a14a68e5
wanghailu <wanghailu0717@163.com>
2023-01-29 06:00:12 +0000

    add logic operation

--- include/core/operator.h
+++ include/core/operator.h
@@ -94,6 +94,16 @@ enum class OpType {
     SquaredDifference,
     Flip,
     Hardtanh,
+    Equal,
+    NotEqual,
+    GreaterThan,
+    GreaterEqual,
+    LessThan,
+    LessEqual,
+    And,
+    Or,
+    Xor,
+    Not,
     //
     MemBound = 300,
 };
@@ -196,6 +206,16 @@ class OpRegistry {
             FOP(SquaredDifference);
             FOP(Flip);
             FOP(Hardtanh);
+            FOP(Equal);
+            FOP(NotEqual);
+            FOP(GreaterThan);
+            FOP(GreaterEqual);
+            FOP(LessThan);
+            FOP(LessEqual);
+            FOP(And);
+            FOP(Or);
+            FOP(Xor);
+            FOP(Not);
             //
             FOP(MemBound);
         default:
