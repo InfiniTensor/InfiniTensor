391b9d16c07459e489ea662123be5b2e69ae499d
YdrMaster <ydrml@hotmail.com>
2023-02-15 14:08:30 +0800

    cleanup
    Signed-off-by: YdrMaster <ydrml@hotmail.com>

--- src/ffi/ffi_infinitensor.cc
+++ src/ffi/ffi_infinitensor.cc
@@ -28,16 +28,13 @@ void init_graph_builder(py::module &m) {
     py::class_<RuntimeObj, std::shared_ptr<RuntimeObj>>(m, "RuntimeObj");
     py::class_<CpuRuntimeObj, std::shared_ptr<CpuRuntimeObj>, RuntimeObj>(
         m, "CpuRuntimeObj");
-    py::class_<Shape>(m, "Shape");
     py::class_<TensorObj, std::shared_ptr<TensorObj>>(m, "TensorObj");
-    py::class_<Tensor>(m, "Tensor");
     py::enum_<ActType>(m, "ActType")
         .value("Linear", ActType::None) // None 是 Python 关键字，不能用
         .value("Relu", ActType::Relu)
         .value("Sigmoid", ActType::Sigmoid)
         .value("Tanh", ActType::Tanh)
         .export_values();
-    py::class_<GraphHandler>(m, "GraphHandler");
     py::class_<Handler>(m, "GraphHandlerObj")
         .def(py::init<Runtime>())
         .def("tensor", py::overload_cast<Shape, int>(&Handler::tensor),
