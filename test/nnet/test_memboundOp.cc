67e9fb9632af492458960f3e6ebd34186c865ff0
huangshuhong <huangsh19@mails.tsinghua.edu.cn>
2023-04-11 23:40:41 +0800

    Fix: test_memboundOp

--- test/nnet/test_memboundOp.cc
+++ test/nnet/test_memboundOp.cc
@@ -18,8 +18,8 @@ TEST(nnet, MemboundOpInterpretation) {
     Tensor w0 = g->addTensor({1, 3, 4}, DataType::UInt32);
     Tensor o0 = g->addTensor({1, 2, 4}, DataType::UInt32);
     g->dataMalloc();
-    i0->copyData(vector<uint32_t>{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12});
-    w0->copyData(vector<uint32_t>{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12});
+    i0->copyin(vector<uint32_t>{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12});
+    w0->copyin(vector<uint32_t>{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12});
     g->addOpWithOutputs<MatmulObj>(i0, w0, o0);
     NMutator nmutator(NMutator::Mode::ToNaiveMembound);
     auto mutations = nmutator.run(g);
@@ -36,7 +36,7 @@ TEST(nnet, MemboundOpInterpretation) {
     EXPECT_EQ(membound->getOpType(), OpType::MemBound);
     auto ans = make_ref<TensorObj>(Shape{1, 2, 4}, DataType::UInt32, runtime);
     ans->dataMalloc();
-    ans->copyData(vector<uint32_t>{38, 44, 50, 56, 83, 98, 113, 128});
+    ans->copyin(vector<uint32_t>{38, 44, 50, 56, 83, 98, 113, 128});
     EXPECT_TRUE(membound->getOutput()->equalData(ans));
 }
 
@@ -49,8 +49,8 @@ TEST(nnet, MemboundOp_Ansor_Codegen) {
     Tensor w0 = g->addTensor({1, 3, 4}, DataType::Float32);
     Tensor o0 = g->addTensor({1, 2, 4}, DataType::Float32);
     g->dataMalloc();
-    i0->copyData(vector<float>{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12});
-    w0->copyData(vector<float>{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12});
+    i0->copyin(vector<float>{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12});
+    w0->copyin(vector<float>{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12});
     g->addOpWithOutputs<MatmulObj>(i0, w0, o0);
     NMutator nmutator(NMutator::Mode::ToNaiveMembound);
     auto mutations = nmutator.run(g);
@@ -67,7 +67,7 @@ TEST(nnet, MemboundOp_Ansor_Codegen) {
     EXPECT_EQ(membound->getOpType(), OpType::MemBound);
     auto ans = make_ref<TensorObj>(Shape{1, 2, 4}, DataType::Float32, cpu);
     ans->dataMalloc();
-    ans->copyData(vector<float>{38, 44, 50, 56, 83, 98, 113, 128});
+    ans->copyin(vector<float>{38, 44, 50, 56, 83, 98, 113, 128});
 
     auto oCpu = gCpu->cloneTensor(membound->getOutput());
     oCpu->printData();
