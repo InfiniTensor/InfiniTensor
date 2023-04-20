8bc2d3e48deb280f9b3cc49c717423d337e99c1e
YdrMaster <ydrml@hotmail.com>
2023-04-20 16:05:33 +0800

    fix: test graph handler
    Signed-off-by: YdrMaster <ydrml@hotmail.com>

--- test/core/test_graph_handler.cc
+++ test/core/test_graph_handler.cc
@@ -8,8 +8,9 @@ TEST(Handler, matmul) {
     auto runtime = NativeCpuRuntimeObj::getInstance();
     auto handler = make_ref<GraphHandlerObj>(runtime);
     auto i = handler->tensor({1, 2, 3}, OnnxDType::UINT32, TensorType::Input);
-    auto w = handler->tensor({1, 3, 4}, OnnxDType::UINT32, TensorType::Input);
-    auto o = handler->tensor({1, 2, 4}, OnnxDType::UINT32, TensorType::Input);
+    auto w =
+        handler->tensor({1, 3, 4}, OnnxDType::UINT32, TensorType::Initialized);
+    auto o = handler->tensor({1, 2, 4}, OnnxDType::UINT32, TensorType::Other);
     handler->matmul(i, w, o, false, false, nullptr, ActType::None);
 }
 
