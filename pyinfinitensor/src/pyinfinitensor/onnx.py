40e6db6608f76a89503c48f7dc1817b127d407c5
Liyan Zheng <liyan-zheng@outlook.com>
2023-04-22 20:28:17 +0800

    Add: tensor FUID in exported ONNX

--- pyinfinitensor/src/pyinfinitensor/onnx.py
+++ pyinfinitensor/src/pyinfinitensor/onnx.py
@@ -639,9 +639,9 @@ class OnnxStub:
                 if name is None:
                     self.count_in += 1
                     if tensor.getTensorType() == backend.TensorType.Input:
-                        name = "input{}".format(self.count_in)
+                        name = f"input{self.count_in}_{tensor.guid()}"
                     else:
-                        name = "weight{}".format(self.count_in)
+                        name = f"weight{self.count_in}_{tensor.guid()}"
                     self.names[tensor] = name
                     if init != None:
                         init.name = name
@@ -706,7 +706,7 @@ class OnnxStub:
                 for it in op.inputs()
             ]
             outputs = [
-                ctx.push_output("{}_{}".format(name, i), it)
+                ctx.push_output(f"{name}_{i}_{it.guid()}", it)
                 for (i, it) in enumerate(op.outputs())
             ]
             if ty == backend.OpType.Conv:
