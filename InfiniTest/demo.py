f99ccc78d913d87c44d7fc40d6cbdbf479f160ef
wanghailu <wanghailu@qiyuanlab.com>
2023-05-10 17:01:02 +0800

    add code

--- InfiniTest/demo.py
+++ InfiniTest/demo.py
@@ -3,6 +3,7 @@ from InfiniTest.accuracy import Accuracy
 from InfiniTest.dump import Dump
 from InfiniTest.pytorch_operator import AddPytorch
 from InfiniTest.performance import Profiling
+from InfiniTest import operator_pb2
 # 导入 pytoch、numpy
 import torch
 import numpy
@@ -26,14 +27,18 @@ def func():
 @pro.hostProfilingWrapper(times=1)
 def test_proto():
     # 构造tensor
-    input1= torch.randint(0,100,(3,4),dtype=torch.int32)
-    input2= torch.randint(0,100,(3,4),dtype=torch.int32)
-    # 调用算子
-    output = torch.add(input1,input2)
+    input1= torch.randint(0,100,(3,4),dtype=torch.float32)
+    input2= torch.randint(0,100,(3,4),dtype=torch.float32)
+    input1_gpu = input1.to(torch.device("cuda"))
+    input2_gpu = input2.to(torch.device("cuda"))
+    # 调用pytorch cpu, gpu算子
+    output_cpu = torch.add(input1,input2)
+    output_gpu = torch.add(input1_gpu, input2_gpu)
+    output_gpu2cpu = output_gpu.to(torch.device("cpu"))
     # 构造测例实例
-    add = AddPytorch([input1,input2],[output])
+    add = AddPytorch([input1,input2],[output_cpu])
     # 序列化为文件
-    add.saveToFile("./prototxt")
+    add.saveToFile("./prototxt", device= operator_pb2.DEVICE_CPU, info = "pytorch 1.10, gpu h100")
     # 构造一个测例实例
     check = AddPytorch()
     # 反序列化测例文件，获得数据
@@ -41,7 +46,9 @@ def test_proto():
     # 构造一个精度检查实例
     acc = Accuracy()
     # 使用方式 0 （完全相等判断）来进行检查
-    acc.computeDifference0("a pass case", output.numpy(), outputs[0].numpy())
+    acc.computeDifference0("a pass case", output_cpu.numpy(), outputs[0].numpy())
+    # 使用方式 1 来进行检查
+    acc.computeDifference1("another pass case", output_cpu.numpy(), output_gpu2cpu.numpy())
     # 注意上述代码糅合了测例生成与测例解析两个过程，实际生产时，这两个过程应当是分开的：测例生成，测例解析&精度判定
 
 # 另外一种使用方式，考虑到有些代码不方便以函数的形式来组装进行装饰器标记，这里提供了Start与End函数来进行起始与末尾的标记
@@ -54,7 +61,4 @@ def another_test():
 func()
 test_proto()
 another_test()
-
-
-
 
