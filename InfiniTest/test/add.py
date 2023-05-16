dc19fad4c809030934757c68da27a64f597ea30b
wanghailu <wanghailu@qiyuanlab.com>
2023-05-16 14:11:16 +0800

    add code

--- InfiniTest/test/add.py
+++ InfiniTest/test/add.py
@@ -4,17 +4,19 @@ from InfiniTest.dump import Dump
 from InfiniTest.pytorch_operator import AddPytorch
 from InfiniTest.performance import Profiling
 from InfiniTest import operator_pb2
-# 导入 pytoch、numpy、datatime、os、argparse
+# 导入 pytoch、numpy、datatime、os、argparse、logging
 import torch
 import numpy
 from datetime import datetime
 import os
 import argparse
+import logging
 # 导入本项目
 from pyinfinitensor.onnx import backend
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--generate_num", default="0", type=int)
+parser.add_argument("--generate_shape", default="2,3", type=str)
 parser.add_argument("--parse", default="False", type=str, choices=["False", "True"])
 args = parser.parse_args()
 
@@ -29,7 +31,10 @@ def generate_case(dir_path:str = "./add/", shape:tuple = (3,4), type = torch.flo
     # 构造测例实例
     add = AddPytorch([x,y],[z])
     # 序列化为文件
-    add.saveToFile(dir_path + str(datetime.now()).replace(" ","_").replace("-","_").replace(":","_").replace(".","_") + ".prototxt", device= operator_pb2.DEVICE_GPU)
+    file_path = dir_path + str(datetime.now()).replace(" ","_").replace("-","_").replace(":","_").replace(".","_") + ".prototxt" 
+    add.saveToFile(file_path, device= operator_pb2.DEVICE_GPU)
+    logging.basicConfig(format='%(asctime)s %(message)s', level=logging.INFO)
+    logging.info("\033[32m" + "[INFO] " + "\033[0m" + ": Save test case to path " + file_path)
 
 def parse_case(dir_path:str="./add/"):
 
@@ -53,8 +58,11 @@ def parse_case(dir_path:str="./add/"):
 
 
 if __name__ == "__main__":
+    shape = args.generate_shape.split(",")
+    shape = [int(i) for i in shape]
+    shape = tuple(shape)
     for i in range(args.generate_num): 
-        generate_case()
+        generate_case(shape = shape)
     if args.parse == "True":
         parse_case()
 
