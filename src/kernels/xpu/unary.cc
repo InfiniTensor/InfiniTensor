221d64e1e1a1550d60b40125a3e821233c0d44db
root <root@localhost.localdomain>
2023-06-01 14:45:25 +0800

    add unary operator

--- src/kernels/xpu/unary.cc
+++ src/kernels/xpu/unary.cc
@@ -71,6 +71,127 @@ class SquareXdnn : public XPUKernelWithoutConfig {
     }
 };
 
+class SqrtXdnn : public XPUKernelWithoutConfig {
+    void compute(const Operator &_op,
+                 const RuntimeObj *_context) const override {
+        auto op = as<UnaryObj>(_op);
+        auto context = dynamic_cast<const XPURuntimeObj *>(_context);
+
+        void *const aData = (op->getInputs(0)->getRawDataPtr<void *>());
+        void *const cData = (op->getOutput()->getRawDataPtr<void *>());
+        auto len = op->getInputs(0)->size();
+
+	auto ret = baidu::xpu::api::sqrt<float>(context->XPUHandle(), (float*)aData, (float*)cData, len);
+	assert(ret == 0);
+	return;
+
+    }
+};
+
+class RsqrtXdnn : public XPUKernelWithoutConfig {
+    void compute(const Operator &_op,
+                 const RuntimeObj *_context) const override {
+        auto op = as<UnaryObj>(_op);
+        auto context = dynamic_cast<const XPURuntimeObj *>(_context);
+
+        void *const aData = (op->getInputs(0)->getRawDataPtr<void *>());
+        void *const cData = (op->getOutput()->getRawDataPtr<void *>());
+        auto len = op->getInputs(0)->size();
+
+	auto ret = baidu::xpu::api::rsqrt<float>(context->XPUHandle(), (float*)aData, (float*)cData, len);
+	assert(ret == 0);
+	return;
+
+    }
+};
+
+class ExpXdnn : public XPUKernelWithoutConfig {
+    void compute(const Operator &_op,
+                 const RuntimeObj *_context) const override {
+        auto op = as<UnaryObj>(_op);
+        auto context = dynamic_cast<const XPURuntimeObj *>(_context);
+
+        void *const aData = (op->getInputs(0)->getRawDataPtr<void *>());
+        void *const cData = (op->getOutput()->getRawDataPtr<void *>());
+        auto len = op->getInputs(0)->size();
+
+	auto ret = baidu::xpu::api::exp<float>(context->XPUHandle(), (float*)aData, (float*)cData, len);
+	assert(ret == 0);
+	return;
+
+    }
+};
+
+class CeilXdnn : public XPUKernelWithoutConfig {
+    void compute(const Operator &_op,
+                 const RuntimeObj *_context) const override {
+        auto op = as<UnaryObj>(_op);
+        auto context = dynamic_cast<const XPURuntimeObj *>(_context);
+
+        void *const aData = (op->getInputs(0)->getRawDataPtr<void *>());
+        void *const cData = (op->getOutput()->getRawDataPtr<void *>());
+        auto len = op->getInputs(0)->size();
+
+	auto ret = baidu::xpu::api::ceil<float>(context->XPUHandle(), (float*)aData, (float*)cData, len);
+	assert(ret == 0);
+	return;
+
+    }
+};
+
+class ClipXdnn : public XPUKernelWithoutConfig {
+    void compute(const Operator &_op,
+                 const RuntimeObj *_context) const override {
+        auto op = as<ClipObj>(_op);
+        auto context = dynamic_cast<const XPURuntimeObj *>(_context);
+
+        void *const aData = (op->getInputs(0)->getRawDataPtr<void *>());
+        void *const cData = (op->getOutput()->getRawDataPtr<void *>());
+        auto len = op->getInputs(0)->size();
+	float min = op->getMin().value();
+	float max = op->getMax().value();
+
+	auto ret = baidu::xpu::api::clip<float>(context->XPUHandle(), (float*)aData, (float*)cData, len, min, max);
+	assert(ret == 0);
+	return;
+
+    }
+};
+
+class FloorXdnn : public XPUKernelWithoutConfig {
+    void compute(const Operator &_op,
+                 const RuntimeObj *_context) const override {
+        auto op = as<UnaryObj>(_op);
+        auto context = dynamic_cast<const XPURuntimeObj *>(_context);
+
+        void *const aData = (op->getInputs(0)->getRawDataPtr<void *>());
+        void *const cData = (op->getOutput()->getRawDataPtr<void *>());
+        auto len = op->getInputs(0)->size();
+
+	auto ret = baidu::xpu::api::floor<float>(context->XPUHandle(), (float*)aData, (float*)cData, len);
+	assert(ret == 0);
+	return;
+
+    }
+};
+
+class NegXdnn : public XPUKernelWithoutConfig {
+    void compute(const Operator &_op,
+                 const RuntimeObj *_context) const override {
+        auto op = as<UnaryObj>(_op);
+        auto context = dynamic_cast<const XPURuntimeObj *>(_context);
+
+        void *const aData = (op->getInputs(0)->getRawDataPtr<void *>());
+        void *const cData = (op->getOutput()->getRawDataPtr<void *>());
+        auto len = op->getInputs(0)->size();
+
+	auto ret = baidu::xpu::api::neg<float>(context->XPUHandle(), (float*)aData, (float*)cData, len);
+	assert(ret == 0);
+	return;
+
+    }
+};
+
 
 REGISTER_KERNEL(Device::XPU, OpType::Relu, DataType::Float32, ReluXdnn,
                 "Relu_xdnn_XPU_Float32");
@@ -80,4 +201,18 @@ REGISTER_KERNEL(Device::XPU, OpType::Tanh, DataType::Float32, TanhXdnn,
                 "Tanh_xdnn_XPU_Float32");
 REGISTER_KERNEL(Device::XPU, OpType::Square, DataType::Float32, SquareXdnn,
                 "Square_xdnn_XPU_Float32");
+REGISTER_KERNEL(Device::XPU, OpType::Sqrt, DataType::Float32, SqrtXdnn,
+                "Sqrt_xdnn_XPU_Float32");
+REGISTER_KERNEL(Device::XPU, OpType::Rsqrt, DataType::Float32, RsqrtXdnn,
+                "Rsqrt_xdnn_XPU_Float32");
+REGISTER_KERNEL(Device::XPU, OpType::Exp, DataType::Float32, ExpXdnn,
+                "Exp_xdnn_XPU_Float32");
+REGISTER_KERNEL(Device::XPU, OpType::Ceil, DataType::Float32, CeilXdnn,
+                "Ceil_xdnn_XPU_Float32");
+REGISTER_KERNEL(Device::XPU, OpType::Clip, DataType::Float32, ClipXdnn,
+                "Clip_xdnn_XPU_Float32");
+REGISTER_KERNEL(Device::XPU, OpType::Floor, DataType::Float32, FloorXdnn,
+                "Floor_xdnn_XPU_Float32");
+REGISTER_KERNEL(Device::XPU, OpType::Neg, DataType::Float32, NegXdnn,
+                "Neg_xdnn_XPU_Float32");
 }; // namespace infini
--- test/kernels/xpu/test_xpu_unary.cc
+++ test/kernels/xpu/test_xpu_unary.cc
@@ -38,10 +38,49 @@ void testUnary(const std::function<void(void *, size_t, DataType)> &generator,
     EXPECT_TRUE(outputCpu->equalData(outputGpu2Cpu));
 }
 
+void testClip(const std::function<void(void *, size_t, DataType)> &generator,
+               const Shape &shape) {
+    // Runtime
+    Runtime cpuRuntime = NativeCpuRuntimeObj::getInstance();
+    auto xpuRuntime = make_ref<XPURuntimeObj>();
+
+    // Build input data on CPU
+    Tensor inputCpu = make_ref<TensorObj>(shape, DataType::Float32, cpuRuntime);
+    inputCpu->dataMalloc();
+    inputCpu->setData(generator);
+    float min = 1.0;
+    float max = 5.0;
+
+    // GPU
+    Graph xpuGraph = make_ref<GraphObj>(xpuRuntime);
+    auto inputGpu = xpuGraph->cloneTensor(inputCpu);
+    auto gpuOp = xpuGraph->addOp<ClipObj>(inputGpu, nullptr, min, max);
+    xpuGraph->dataMalloc();
+    xpuRuntime->run(xpuGraph);
+    auto outputGpu = gpuOp->getOutput();
+    auto outputGpu2Cpu = outputGpu->clone(cpuRuntime);
+    // CPU
+    Graph cpuGraph = make_ref<GraphObj>(cpuRuntime);
+    auto cpuOp = cpuGraph->addOp<ClipObj>(inputCpu, nullptr, min, max);
+    cpuGraph->dataMalloc();
+    cpuRuntime->run(cpuGraph);
+    auto outputCpu = cpuOp->getOutput();
+    // Check
+    EXPECT_TRUE(outputCpu->equalData(outputGpu2Cpu));
+}
+
 TEST(xdnn_Unary, run) {
     testUnary<ReluObj>(IncrementalGenerator(), Shape{1, 2, 2, 3});
     testUnary<SigmoidObj>(IncrementalGenerator(), Shape{1, 2, 2, 3});
     testUnary<TanhObj>(IncrementalGenerator(), Shape{1, 2, 2, 3});
+    //testUnary<SquareObj>(IncrementalGenerator(), Shape{1, 2, 2, 3});
+    //testUnary<SqrtObj>(IncrementalGenerator(), Shape{1, 2, 2, 3});
+    //testUnary<RsqrtObj>(IncrementalGenerator(), Shape{1, 2, 2, 3});
+    //testUnary<ExpObj>(IncrementalGenerator(), Shape{1, 2, 2, 3});
+    //testUnary<CeilObj>(IncrementalGenerator(), Shape{1, 2, 2, 3});
+    //testUnary<FloorObj>(IncrementalGenerator(), Shape{1, 2, 2, 3});
+    //testUnary<NegObj>(IncrementalGenerator(), Shape{1, 2, 2, 3});
+    //testUnary<ClipObj>(IncrementalGenerator(), Shape{1, 2, 2, 3});
 }
 
 } // namespace infini
