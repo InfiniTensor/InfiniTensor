440df742f3cad944adb5c338c079304103b73a27
YdrMaster <ydrml@hotmail.com>
2023-07-20 13:34:29 +0800

    feat: 添加构图用的方法
    Signed-off-by: YdrMaster <ydrml@hotmail.com>

--- include/core/memory_graph.h
+++ include/core/memory_graph.h
@@ -61,7 +61,7 @@ struct Outlet {
 };
 
 /// @brief 算子属性的基类。
-class OpAttibutes {};
+class MemoryOpAttibutes {};
 
 /// @brief 访存算子。
 struct MemoryOperator {
@@ -70,26 +70,12 @@ struct MemoryOperator {
     /// @brief 算子类型。
     MemoryOpType opType;
     /// @brief 算子属性。
-    std::unique_ptr<OpAttibutes> attributes;
+    std::unique_ptr<MemoryOpAttibutes> attributes;
     /// @brief 算子输入。
     std::vector<Inlet> inputs;
     /// @brief 算子输出。
     std::vector<Outlet> outputs;
 };
-
-/// @brief 检查一个访存模式是否连续。
-/// @param mmm 访存模式。
-/// @return 如果连续，返回 `true`。
-bool check_dense(std::vector<MemoryMode> const &mmm) {
-    if (mmm[0].stride != 1) {
-        return false;
-    }
-    for (size_t i = 1; i < mmm.size(); ++i) {
-        if (mmm[i].stride != mmm[i - 1].size * mmm[i - 1].stride)
-            return false;
-    }
-    return true;
-}
 
 /// @brief 访存图。
 class MemoryGraphObj {
@@ -98,45 +84,26 @@ class MemoryGraphObj {
 
   public:
     /// @brief 构造一个空的访存图。
-    MemoryGraphObj()
-        : ops{
-              // 输入输出自动在 0、1 号，其他算子按拓扑序。
-              MemoryOperator{0, MemoryOpType::Input, {}, {}},
-              MemoryOperator{1, MemoryOpType::Output, {}, {}},
-          } {}
+    MemoryGraphObj();
 
     /// @brief 添加新的图输入。
     /// @param shape 访存图的输入必须是连续的。
     /// @param dt 数据类型。
     /// @return 新输入的位置。
-    OutletPos pushInput(std::vector<dim_t> shape, DataType dt) {
-        // 生成连续的访存模式。
-        auto memory = std::vector<MemoryMode>(shape.size());
-        memory[0] = {shape[0], 1};
-        for (size_t i = 1; i < shape.size(); ++i) {
-            memory[i] = {shape[i], shape[i - 1] * memory[i - 1].stride};
-        }
-        // 填写输入。
-        auto &inputs = ops[0].outputs;
-        auto slot = inputs.size();
-        inputs.push_back({{}, std::move(memory), dt});
-        return {0, slot};
-    }
+    OutletPos pushInput(std::vector<dim_t> shape, DataType dt);
 
     /// @brief 添加新的图输出。
-    /// @param pos 输出数据位置。
-    void setOutput(OutletPos pos) {
-        // 输入的直接又输出不合常理。
-        // TODO 但是可能需要支持空的访存图。
-        assert(pos.opIdx != 0);
-        // 检查访存连续性。只有连续访存的数据才能作为输出。
-        auto &outlet = ops[pos.opIdx].outputs[pos.slot];
-        assert(check_dense(outlet.memoryMode));
-        // 填写输出。
-        auto &outputs = ops[1].inputs;
-        auto slot = outputs.size();
-        outlet.targets.push_back({1, slot});
-        outputs.push_back(Inlet{pos, outlet.memoryMode});
-    }
+    /// @param 输出数据位置。
+    void setOutput(OutletPos);
+
+    /// @brief 添加算子。
+    /// @param opType 算子类型。
+    /// @param attributes 算子属性。
+    /// @param inputs 算子输入。
+    /// @param outputs 算子输出。
+    /// @return 算子的输出位置。
+    std::vector<OutletPos>
+    push_op(MemoryOpType opType, std::unique_ptr<MemoryOpAttibutes> attributes,
+            std::vector<Inlet> inputs, std::vector<Outlet> outputs);
 };
 } // namespace infini
