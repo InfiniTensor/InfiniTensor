8d347147e7b1bfe6c6e4e876d7a94ea55aa7dc68
YdrMaster <ydrml@hotmail.com>
2023-07-21 10:52:49 +0800

    feat: format py files
    Signed-off-by: YdrMaster <ydrml@hotmail.com>

--- scripts/format.py
+++ scripts/format.py
@@ -3,6 +3,7 @@ import sys
 from pathlib import Path
 
 c_style_file = [".h", ".hh", ".hpp", ".c", ".cc", ".cpp", ".cxx", ".cu", ".mlu"]
+py_file = ".py"
 proj_path = Path(sys.path[0]).parent
 
 for line in (
@@ -17,5 +18,8 @@ for line in (
             file = Path(proj_path.joinpath(line[len(pre) :].strip()))
             if file.suffix in c_style_file:
                 run(f"clang-format-14 -i {file}", cwd=proj_path, shell=True)
+                run(f"git add {file}", cwd=proj_path, shell=True)
+            elif file.suffix == py_file:
+                run(f"black {file}", cwd=proj_path, shell=True)
                 run(f"git add {file}", cwd=proj_path, shell=True)
         break
