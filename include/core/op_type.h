1d775c4cbcff2cbd84220b0af338e97f5dfe10f2
YdrMaster <ydrml@hotmail.com>
2023-07-24 11:19:04 +0800

    feat: 添加新的 OpType 定义
    Signed-off-by: YdrMaster <ydrml@hotmail.com>

---
+++ include/core/op_type.h
@@ -0,0 +1,435 @@
+﻿#pragma once
+#ifndef OP_TYPE_H
+#define OP_TYPE_H
+
+#include "core/tensor.h"
+#include <string>
+#include <unordered_set>
+
+namespace infini {
+
+struct NewOpType {
+    // Clang-format is ambiguous in formating of comment alignment.
+    // In order to disambiguate, it is necessary to comment all enum elements.
+    enum {
+        Abs,                // Unary
+        Acos,               // Unary
+        Acosh,              // Unary
+        Add,                // Binary
+        And,                // Binary
+        ArgMax,             //
+        Asin,               // Binary
+        Asinh,              // Binary
+        Atan,               // Binary
+        Atanh,              // Binary
+        AveragePool,        // Pool
+        BatchNormalization, //
+        Bernoulli,          //
+        BitShift,           // Binary
+        BitwiseAnd,         // Binary
+        BitwiseNot,         // Binary
+        BitwiseOr,          // Binary
+        BitwiseXor,         // Binary
+        BlackmanWindow,     //
+        Cast,               // Unary
+        CastLike,           //
+        Ceil,               // Unary
+        Celu,               //
+        CenterCropPad,      //
+        Clip,               // Unary
+        Col2lm,
+        Compress,
+        Concat,
+        ConcatFromSequence,
+        ConstantOfShape,
+        Conv,
+        ConvInteger,
+        ConvTranspose,
+        Cos,  // Unary
+        Cosh, // Unary
+        CumSum,
+        DFT,
+        DeformConv,
+        DepthToSpace,
+        DequantizeLinear,
+        Det,
+        Div, // Binary
+        Dropout,
+        DynamicQuantizeLinear,
+        Einsum,
+        Elu,
+        Equal, // Compair
+        Erf,   // Unary
+        Exp,   // Unary
+        Expand,
+        EyeLike,
+        Flatten,
+        Floor, // Unary
+        GRU,
+        Gather,
+        GatherElements,
+        GatherND,
+        Gemm,
+        GlobalAveragePool, // GlobalPool
+        GlobalLpPool,      // GlobalPool
+        GlobalMaxPool,     // GlobalPool
+        Greater,           // Compair
+        GreaterOrEqual,    // Compair
+        GridSample,
+        GroupNormalization,
+        HammingWindow,
+        HannWindow,
+        HardSigmoid,
+        HardSwish,
+        Hardmax,
+        Identity,
+        If,
+        InstanceNormalization,
+        IsInf,
+        IsNaN,
+        LRN,
+        LSTM,
+        LayerNormalization,
+        LeakyRelu,
+        Less,        // Compair
+        LessOrEqual, // Compair
+        Log,         // Unary
+        LogSoftmax,
+        Loop,
+        LpNormalization,
+        LpPool,
+        MatMul,
+        MatMulInteger,
+        Max,
+        MaxPool,
+        MaxRoiPool,
+        MaxUnpool,
+        Mean,
+        MeanVarianceNormalization,
+        MelWeightMatrix,
+        Min,
+        Mish,
+        Mod, // Binary
+        Mul, // Binary
+        Multinomial,
+        Neg, // Unary
+        NegativeLogLikelihoodLoss,
+        NonMaxSuppression,
+        NonZero,
+        Not, // Unary
+        OneHot,
+        Optional,
+        OptionalGetElement,
+        OptionalHasElement,
+        Or, // Binary
+        PRelu,
+        Pad,
+        Pow, // Binary
+        QLinearConv,
+        QLinearMatMul,
+        QuantizeLinear,
+        RNN,
+        RandomNormal,
+        RandomNormalLike,
+        RandomUniform,
+        RandomUniformLike,
+        Range,
+        Reciprocal,
+        ReduceL1,        // Reduce
+        ReduceL2,        // Reduce
+        ReduceLogSum,    // Reduce
+        ReduceLogSumExp, // Reduce
+        ReduceMax,       // Reduce
+        ReduceMean,      // Reduce
+        ReduceMin,       // Reduce
+        ReduceProd,      // Reduce
+        ReduceSum,       // Reduce
+        ReduceSumSquare, // Reduce
+        Relu,            // Unary
+        Reshape,
+        Resize,
+        ReverseSequence,
+        RoiAlign,
+        Round, // Unary
+        STFT,
+        Scan,
+        Scatter,
+        ScatterElements,
+        ScatterND,
+        Selu,
+        SequenceAt,
+        SequenceConstruct,
+        SequenceEmpty,
+        SequenceErase,
+        SequenceInsert,
+        SequenceLength,
+        SequenceMap,
+        Shape,
+        Shrink,
+        Sigmoid,
+        Sign,
+        Sin,  // Unary
+        Sinh, // Unary
+        Size,
+        Slice,
+        Softmax,
+        SoftmaxCrossEntropyLoss,
+        Softplus,
+        Softsign,
+        SpaceToDepth,
+        Split,
+        SplitToSequence,
+        Sqrt,
+        Squeeze,
+        StringNormalizer,
+        Sub,  // Binary
+        Sum,  //
+        Tan,  // Unary
+        Tanh, // unary
+        TfIdfVectorizer,
+        ThresholdedRelu,
+        Tile,
+        TopK,
+        Transpose,
+        Trilu,
+        Unique,
+        Unsqueeze,
+        Upsample,
+        Where,
+        Xor, // Binary
+    } type;
+
+    const char *ToString() const;
+    bool isUnary() const;
+    bool isBinary() const;
+    bool isElementWise() const;
+    bool isCompair() const;
+    bool isPool() const;
+    bool isGlobalPool() const;
+};
+
+enum class OpType {
+    Unknown = 0,
+    // linear
+    Conv = 100,
+    ConvBackwardFilter,
+    ConvBackwardData,
+    Matmul,
+    ConvTrans,
+    ConvTransNHWC,
+    G2BMM,
+    GBMM,
+    Pad,
+    Slice,
+    Concat,
+    Split,
+    Transpose,
+    Extend,
+    MaxPool,
+    AvgPool,
+    Add,
+    Sub,
+    Mul,
+    Div,
+    Pow,
+    Gather,
+    ReduceMean,
+    Reshape,
+    Flatten,
+    Identity,
+    // element wise
+    BatchNorm = 200,
+    Softmax,
+    Activation,
+    Relu,
+    ReluBackward,
+    PRelu,
+    Sigmoid,
+    SigmoidBackward,
+    Tanh,
+    TanhBackward,
+    Abs,
+    Sin,
+    Cos,
+    Tan,
+    ASin,
+    ACos,
+    ATan,
+    SinH,
+    CosH,
+    TanH,
+    ASinH,
+    ACosH,
+    ATanH,
+    Resize,
+    Arange,
+    Shape,
+    Copy,
+    Ceil,
+    Floor,
+    Clip,
+    Erf,
+    Exp,
+    Fill,
+    Log,
+    L2Loss,
+    Maximum,
+    Minimum,
+    MSELoss,
+    Neg,
+    Power,
+    Reciprocal,
+    Sqrt,
+    Rsqrt,
+    Cast,
+    FloorDiv,
+    FloorMod,
+    Det,
+    Round,
+    Square,
+    SquaredDifference,
+    Hardtanh,
+    Equal,
+    NotEqual,
+    GreaterThan,
+    GreaterEqual,
+    LessThan,
+    LessEqual,
+    And,
+    Or,
+    Xor,
+    Not,
+    BitAnd,
+    BitOr,
+    BitXor,
+    BitNot,
+    BitLeftShift,
+    BitRightShift,
+    Dropout,
+    UnaryKernel,
+    //
+    MemBound = 300,
+    MemoryGraph,
+};
+
+class OpRegistry {
+  public:
+    static std::string getOpName(OpType opType) {
+#define FOP(op)                                                                \
+    case OpType::op:                                                           \
+        return #op
+
+        switch (opType) {
+            FOP(Unknown);
+            // linear
+            FOP(Conv);
+            FOP(ConvBackwardFilter);
+            FOP(ConvBackwardData);
+            FOP(Matmul);
+            FOP(ConvTrans);
+            FOP(G2BMM);
+            FOP(GBMM);
+            FOP(Pad);
+            FOP(Slice);
+            FOP(Concat);
+            FOP(Split);
+            FOP(Transpose);
+            FOP(Extend);
+            FOP(MaxPool);
+            FOP(AvgPool);
+            FOP(Add);
+            FOP(Sub);
+            FOP(Mul);
+            FOP(Div);
+            FOP(Pow);
+            FOP(Gather);
+            FOP(ReduceMean);
+            FOP(Reshape);
+            FOP(Identity);
+            FOP(Shape);
+            // element wise
+            FOP(BatchNorm);
+            FOP(Softmax);
+            FOP(Activation);
+            FOP(Relu);
+            FOP(ReluBackward);
+            FOP(PRelu);
+            FOP(Sigmoid);
+            FOP(SigmoidBackward);
+            FOP(Tanh);
+            FOP(TanhBackward);
+            FOP(Abs);
+            FOP(Sin);
+            FOP(Cos);
+            FOP(Tan);
+            FOP(ASin);
+            FOP(ACos);
+            FOP(ATan);
+            FOP(SinH);
+            FOP(CosH);
+            FOP(TanH);
+            FOP(ASinH);
+            FOP(ACosH);
+            FOP(ATanH);
+            FOP(Copy);
+            FOP(Ceil);
+            FOP(Floor);
+            FOP(Clip);
+            FOP(Erf);
+            FOP(Exp);
+            FOP(Fill);
+            FOP(Log);
+            FOP(L2Loss);
+            FOP(Maximum);
+            FOP(Minimum);
+            FOP(MSELoss);
+            FOP(Neg);
+            FOP(Power);
+            FOP(Reciprocal);
+            FOP(Sqrt);
+            FOP(Rsqrt);
+            FOP(Cast);
+            FOP(FloorDiv);
+            FOP(FloorMod);
+            FOP(Det);
+            FOP(Round);
+            FOP(Square);
+            FOP(SquaredDifference);
+            FOP(Hardtanh);
+            FOP(Equal);
+            FOP(NotEqual);
+            FOP(GreaterThan);
+            FOP(GreaterEqual);
+            FOP(LessThan);
+            FOP(LessEqual);
+            FOP(And);
+            FOP(Or);
+            FOP(Xor);
+            FOP(Not);
+            FOP(BitAnd);
+            FOP(BitOr);
+            FOP(BitXor);
+            FOP(BitNot);
+            FOP(BitLeftShift);
+            FOP(BitRightShift);
+            FOP(UnaryKernel);
+            //
+            FOP(MemBound);
+        default:
+            IT_ASSERT(false);
+            break;
+        }
+#undef FOP
+    }
+};
+
+enum class ActType {
+    None,
+    Relu,
+    Sigmoid,
+    Tanh,
+};
+
+} // namespace infini
+
+#endif // OP_TYPE_H
---
+++ src/core/op_type.cc
@@ -0,0 +1,244 @@
+﻿#include "core/op_type.h"
+
+namespace infini {
+const char *NewOpType::ToString() const {
+#define CASE(NAME)                                                             \
+    case NewOpType::NAME:                                                      \
+        return #NAME
+
+    switch (type) {
+        CASE(Abs);
+        CASE(Acos);
+        CASE(Acosh);
+        CASE(Add);
+        CASE(And);
+        CASE(ArgMax);
+        CASE(Asin);
+        CASE(Asinh);
+        CASE(Atan);
+        CASE(Atanh);
+        CASE(AveragePool);
+        CASE(BatchNormalization);
+        CASE(Bernoulli);
+        CASE(BitShift);
+        CASE(BitwiseAnd);
+        CASE(BitwiseNot);
+        CASE(BitwiseOr);
+        CASE(BitwiseXor);
+        CASE(BlackmanWindow);
+        CASE(Cast);
+        CASE(CastLike);
+        CASE(Ceil);
+        CASE(Celu);
+        CASE(CenterCropPad);
+        CASE(Clip);
+        CASE(Col2lm);
+        CASE(Compress);
+        CASE(Concat);
+        CASE(ConcatFromSequence);
+        CASE(ConstantOfShape);
+        CASE(Conv);
+        CASE(ConvInteger);
+        CASE(ConvTranspose);
+        CASE(Cos);
+        CASE(Cosh);
+        CASE(CumSum);
+        CASE(DFT);
+        CASE(DeformConv);
+        CASE(DepthToSpace);
+        CASE(DequantizeLinear);
+        CASE(Det);
+        CASE(Div);
+        CASE(Dropout);
+        CASE(DynamicQuantizeLinear);
+        CASE(Einsum);
+        CASE(Elu);
+        CASE(Equal);
+        CASE(Erf);
+        CASE(Exp);
+        CASE(Expand);
+        CASE(EyeLike);
+        CASE(Flatten);
+        CASE(Floor);
+        CASE(GRU);
+        CASE(Gather);
+        CASE(GatherElements);
+        CASE(GatherND);
+        CASE(Gemm);
+        CASE(GlobalAveragePool);
+        CASE(GlobalLpPool);
+        CASE(GlobalMaxPool);
+        CASE(Greater);
+        CASE(GreaterOrEqual);
+        CASE(GridSample);
+        CASE(GroupNormalization);
+        CASE(HammingWindow);
+        CASE(HannWindow);
+        CASE(HardSigmoid);
+        CASE(HardSwish);
+        CASE(Hardmax);
+        CASE(Identity);
+        CASE(If);
+        CASE(InstanceNormalization);
+        CASE(IsInf);
+        CASE(IsNaN);
+        CASE(LRN);
+        CASE(LSTM);
+        CASE(LayerNormalization);
+        CASE(LeakyRelu);
+        CASE(Less);
+        CASE(LessOrEqual);
+        CASE(Log);
+        CASE(LogSoftmax);
+        CASE(Loop);
+        CASE(LpNormalization);
+        CASE(LpPool);
+        CASE(MatMul);
+        CASE(MatMulInteger);
+        CASE(Max);
+        CASE(MaxPool);
+        CASE(MaxRoiPool);
+        CASE(MaxUnpool);
+        CASE(Mean);
+        CASE(MeanVarianceNormalization);
+        CASE(MelWeightMatrix);
+        CASE(Min);
+        CASE(Mish);
+        CASE(Mod);
+        CASE(Mul);
+        CASE(Multinomial);
+        CASE(Neg);
+        CASE(NegativeLogLikelihoodLoss);
+        CASE(NonMaxSuppression);
+        CASE(NonZero);
+        CASE(Not);
+        CASE(OneHot);
+        CASE(Optional);
+        CASE(OptionalGetElement);
+        CASE(OptionalHasElement);
+        CASE(Or);
+        CASE(PRelu);
+        CASE(Pad);
+        CASE(Pow);
+        CASE(QLinearConv);
+        CASE(QLinearMatMul);
+        CASE(QuantizeLinear);
+        CASE(RNN);
+        CASE(RandomNormal);
+        CASE(RandomNormalLike);
+        CASE(RandomUniform);
+        CASE(RandomUniformLike);
+        CASE(Range);
+        CASE(Reciprocal);
+        CASE(ReduceL1);
+        CASE(ReduceL2);
+        CASE(ReduceLogSum);
+        CASE(ReduceLogSumExp);
+        CASE(ReduceMax);
+        CASE(ReduceMean);
+        CASE(ReduceMin);
+        CASE(ReduceProd);
+        CASE(ReduceSum);
+        CASE(ReduceSumSquare);
+        CASE(Relu);
+        CASE(Reshape);
+        CASE(Resize);
+        CASE(ReverseSequence);
+        CASE(RoiAlign);
+        CASE(Round);
+        CASE(STFT);
+        CASE(Scan);
+        CASE(Scatter);
+        CASE(ScatterElements);
+        CASE(ScatterND);
+        CASE(Selu);
+        CASE(SequenceAt);
+        CASE(SequenceConstruct);
+        CASE(SequenceEmpty);
+        CASE(SequenceErase);
+        CASE(SequenceInsert);
+        CASE(SequenceLength);
+        CASE(SequenceMap);
+        CASE(Shape);
+        CASE(Shrink);
+        CASE(Sigmoid);
+        CASE(Sign);
+        CASE(Sin);
+        CASE(Sinh);
+        CASE(Size);
+        CASE(Slice);
+        CASE(Softmax);
+        CASE(SoftmaxCrossEntropyLoss);
+        CASE(Softplus);
+        CASE(Softsign);
+        CASE(SpaceToDepth);
+        CASE(Split);
+        CASE(SplitToSequence);
+        CASE(Sqrt);
+        CASE(Squeeze);
+        CASE(StringNormalizer);
+        CASE(Sub);
+        CASE(Sum);
+        CASE(Tan);
+        CASE(Tanh);
+        CASE(TfIdfVectorizer);
+        CASE(ThresholdedRelu);
+        CASE(Tile);
+        CASE(TopK);
+        CASE(Transpose);
+        CASE(Trilu);
+        CASE(Unique);
+        CASE(Unsqueeze);
+        CASE(Upsample);
+        CASE(Where);
+        CASE(Xor);
+    default:
+        IT_ASSERT(false);
+    }
+
+#undef CASE
+}
+
+bool NewOpType::isUnary() const {
+    static const std::unordered_set<decltype(type)> set{
+        Abs,  Acos,  Acosh,   Asin, Asinh, Atan,  Atanh, Cast, Ceil,
+        Clip, Cos,   Cosh,    Erf,  Exp,   Floor, Log,   Neg,  Not,
+        Relu, Round, Sigmoid, Sin,  Sinh,  Sqrt,  Tan,   Tanh,
+    };
+
+    return set.find(type) != set.end();
+}
+
+bool NewOpType::isBinary() const {
+    static const std::unordered_set<decltype(type)> set{
+        Add, And, BitShift, BitwiseAnd, BitwiseNot, BitwiseOr, BitwiseXor,
+        Div, Mod, Mul,      Or,         Pow,        Sub,       Xor,
+    };
+
+    return set.find(type) != set.end() || isCompair();
+}
+
+bool NewOpType::isElementWise() const { return isUnary() || isBinary(); }
+
+bool NewOpType::isCompair() const {
+    static const std::unordered_set<decltype(type)> set{
+        Equal, Greater, GreaterOrEqual, Less, LessOrEqual,
+    };
+
+    return set.find(type) != set.end();
+}
+
+bool NewOpType::isPool() const {
+    static const std::unordered_set<decltype(type)> set{};
+
+    return set.find(type) != set.end();
+}
+
+bool NewOpType::isGlobalPool() const {
+    static const std::unordered_set<decltype(type)> set{
+        GlobalAveragePool, GlobalLpPool, GlobalMaxPool};
+
+    return set.find(type) != set.end();
+}
+
+} // namespace infini
