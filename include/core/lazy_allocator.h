71df36c804575230d7695cef62c1018566690ef1
kilinchange <kilinchange@163.com>
2023-07-25 17:36:10 +0800

    - basic function of lazy_allocator, remaining test

--- include/core/lazy_allocator.h
+++ include/core/lazy_allocator.h
@@ -8,42 +8,73 @@ namespace infini {
 
 class LazyAllocator {
     protected:
+        // 用于调用 runtime->allocBlob() 函数进行实际的内存分配
         Runtime runtime;
+
+        // 记录当前使用的内存大小
+        size_t used;
 
         // 记录推理过程中的内存峰值
         size_t peak;
 
+        // 内存对齐
+        size_t alignment;
+
+        struct freeBlockInfo {
+            size_t addr;
+            size_t blockSize;
+            freeBlockInfo(size_t addr, size_t blockSize)
+                : addr(addr), blockSize(blockSize) {}
+        };
+
+        struct cmpFreeBlockInfo {
+            bool operator()(const freeBlockInfo &a, const freeBlockInfo &b) const {
+                return (a.blockSize != b.blockSize) ? (a.blockSize < b.blockSize)
+                                                    : (a.addr < b.addr);
+            }
+        };
+
         // 空闲平衡树，维护所有空闲内存块
         // key: 空闲内存块大小，val: 内存块起始地址偏移量
-        std::map<size_t, size_t> freeSizeToHeadAddr;
+        std::set<freeBlockInfo, cmpFreeBlockInfo> freeBlocks;
+        // map<size_t, size_t> freeSizeToHeadAddr;
 
         // 空闲块起始地址集合，维护所有空闲内存块的起始地址偏移量，用于碎片回收
-        std::unordered_set<size_t> freeBlocksHeadAddr;
+        std::unordered_map<size_t, size_t> headAddrToBlockSize;
         
         // 空闲块结尾地址集合，维护所有空闲内存块的结尾地址偏移量，用于碎片回收
-        std::unordered_set<size_t> freeBlocksTailAddr; 
+        std::unordered_map<size_t, size_t> tailAddrToBlockSize;
+
     public:
-        LazyAllocator(Runtime runtime);
+        LazyAllocator(Runtime runtime, size_t alignment);
         
         virtual ~LazyAllocator();
+
+        // 功能：初始化成员变量 peak=size。
+        // 输入参数：
+        //    size：计算图中的常量 Tensor（模型权重等）所需内存大小。
+        void init(size_t size);
 
         // 功能：模拟分配内存。   
         // 输入参数：
         //     size：需要分配的内存大小。
         //     alignment：内存对齐量。
         // 返回值：所分配内存块的起始地址偏移量。 
-        void init(size_t size);
-
         size_t alloc(size_t size, size_t alignment);
 
         // 功能：模拟释放内存。
         // 输入参数：
         //     addr：需要释放的内存起始地址偏移量。
-        void free(size_t addr);
+        //     size：需要释放的 Tensor 大小
+        void free(size_t addr, size_t size);
 
         // 功能：进行实际的内存分配
         // 返回值：指向所分配内存起始地址的指针
         Blob ptr();
+
+        // 功能：内存对齐，向上取整
+        // 返回值：对齐后的尾地址
+        size_t LazyAllocator::getAlignedTailAddr(size_t baseAddr);
 };
 
 } // namespace infini
--- src/core/lazy_allocator.cc
+++ src/core/lazy_allocator.cc
@@ -1,24 +1,84 @@
 #include "core/lazy_allocator.h"
+#include <utility>
 
 namespace infini {
-    LazyAllocator::LazyAllocator(Runtime runtime) : runtime(runtime){}
+    LazyAllocator::LazyAllocator(Runtime runtime, size_t alignment) : runtime(runtime), alignment(alignment) {}
 
     LazyAllocator::~LazyAllocator() {}
 
     void LazyAllocator::init(size_t size) {
         peak = size;
+        used = size;
     }
 
     size_t LazyAllocator::alloc(size_t size, size_t alignment) {
-        return 0;
+        // 这里保守考虑，使用 size + alignment 作为要寻找的空闲块大小
+        auto it = freeBlocks.lower_bound(freeBlockInfo((size_t)0, size + alignment));
+
+        size_t retAddr = peak;
+        if (it != freeBlocks.end()) {
+            // 找到了可以分配的空内存块
+            size_t blockSize = it->blockSize;
+            retAddr = it->addr;
+            size_t tailAddr = getAlignedTailAddr(retAddr);
+            // 更新空闲块地址集合
+            headAddrToBlockSize.erase(retAddr);
+            tailAddrToBlockSize.erase(tailAddr);
+            // 内存块分裂
+            if (blockSize > tailAddr - retAddr) {
+                freeBlockInfo newBlock = freeBlockInfo(tailAddr, blockSize - (tailAddr - retAddr));
+                headAddrToBlockSize[tailAddr] = newBlock.blockSize;
+                tailAddrToBlockSize[retAddr + blockSize] = newBlock.blockSize;
+                freeBlocks.insert(newBlock);
+            }
+            // 更新空闲平衡树
+            freeBlocks.erase(it);
+        } else {
+            // 已分配的内存空间大小不足以进行再分配，需要扩充
+            retAddr = peak;
+            peak = getAlignedTailAddr(peak + size);
+        }
+        return retAddr;
     }
 
-    void LazyAllocator::free(size_t addr) {
-
+    void LazyAllocator::free(size_t addr, size_t size) {
+        auto tailAddr = getAlignedTailAddr(addr + size);
+        size_t currBlockSize = tailAddr - addr;
+        freeBlockInfo block = freeBlockInfo(addr, tailAddr - addr);
+        headAddrToBlockSize[addr] = currBlockSize;
+        tailAddrToBlockSize[tailAddr] = currBlockSize;
+        auto preFreeBlockIter = tailAddrToBlockSize.find(addr);
+        auto subFreeBlockIter = headAddrToBlockSize.find(tailAddr);
+        if (preFreeBlockIter != tailAddrToBlockSize.end()) {
+            // 需要释放的内存块的头地址是某个空闲块的尾，将二者进行合并
+            size_t preBlockSize = preFreeBlockIter->second;
+            headAddrToBlockSize.erase(addr);
+            headAddrToBlockSize[addr - preBlockSize] += currBlockSize;
+            tailAddrToBlockSize.erase(addr);
+            tailAddrToBlockSize[tailAddr] += preBlockSize;
+            addr -= preBlockSize;
+            block.addr = addr;
+            block.blockSize += preBlockSize;
+        }
+        if (subFreeBlockIter != headAddrToBlockSize.end()) {
+            // 需要释放的内存块的尾地址是某个空闲块的头，将二者进行合并
+            auto subBlockSize = subFreeBlockIter->second;
+            headAddrToBlockSize.erase(tailAddr);
+            headAddrToBlockSize[addr] += subBlockSize;
+            tailAddrToBlockSize.erase(tailAddr);
+            tailAddrToBlockSize[tailAddr + subBlockSize] += currBlockSize;
+            tailAddr += subBlockSize;
+            block.blockSize += subBlockSize;
+        }
+        freeBlocks.insert(block);
     }
 
     Blob LazyAllocator::ptr() {
         return runtime->allocBlob(peak);
+    }
+
+    size_t LazyAllocator::getAlignedTailAddr(size_t baseAddr) {
+        return ((baseAddr - 1) / alignment + 1) * alignment;
     }
 
 } // namespace infini
