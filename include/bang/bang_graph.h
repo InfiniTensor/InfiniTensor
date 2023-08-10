8737f5773d8201f76421aceaa2c194947dde7f47
wanghailu <wanghailu@qiyuanlab.com>
2023-08-10 15:09:19 +0800

    commit for save

---
+++ include/bang/bang_graph.h
@@ -0,0 +1,23 @@
+#pragma once
+#include <string>
+#include <vector>
+
+namespace infini {
+
+enum class BangOpName {
+  None,
+  Add,
+  Sub,
+  Mul,
+  Relu,
+  Sigmoid,
+};
+
+class BangOperator {
+}; 
+
+class BangGraph {
+  public:
+};
+
+}
