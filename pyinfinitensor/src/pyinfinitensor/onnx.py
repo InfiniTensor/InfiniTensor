c0d9b6b36a355ff3bd01be8f647df26251191d3e
constroy <constroy.li@gmail.com>
2023-08-18 15:05:02 +0800

    fix format of onnx.py

--- pyinfinitensor/src/pyinfinitensor/onnx.py
+++ pyinfinitensor/src/pyinfinitensor/onnx.py
@@ -573,23 +573,23 @@ class OnnxStub:
                     )
                 elif node.op_type == "AllReduceSum":
                     tensors[node.output[0]] = self.handler.allReduceSum(
-                    tensors[node.input[0]],
-                    tensors.get(node.output[0]),
+                        tensors[node.input[0]],
+                        tensors.get(node.output[0]),
                     )
                 elif node.op_type == "AllReduceProd":
                     tensors[node.output[0]] = self.handler.allReduceProd(
-                    tensors[node.input[0]],
-                    tensors.get(node.output[0]),
+                        tensors[node.input[0]],
+                        tensors.get(node.output[0]),
                     )
                 elif node.op_type == "AllReduceMin":
                     tensors[node.output[0]] = self.handler.allReduceMin(
-                    tensors[node.input[0]],
-                    tensors.get(node.output[0]),
+                        tensors[node.input[0]],
+                        tensors.get(node.output[0]),
                     )
                 elif node.op_type == "AllReduceMax":
                     tensors[node.output[0]] = self.handler.allReduceMax(
-                    tensors[node.input[0]],
-                    tensors.get(node.output[0]),
+                        tensors[node.input[0]],
+                        tensors.get(node.output[0]),
                     )
                 elif node.op_type == "AllReduceAvg":
                     tensors[node.output[0]] = self.handler.allReduceAvg(
