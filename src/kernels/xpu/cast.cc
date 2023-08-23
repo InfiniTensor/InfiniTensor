1971ed5e7a08dedce71f03d5dded8fd86b3cd375
Bolun <bolunz@u.nus.edu>
2023-08-23 16:41:51 +0800

    add more cast types for xpu

--- src/kernels/xpu/cast.cc
+++ src/kernels/xpu/cast.cc
@@ -16,11 +16,56 @@ class CastXdnn : public XPUKernelWithoutConfig {
 
 	int ret = 0;
 	switch (type) {
+		case CastType::Float2Float16:
+			ret = baidu::xpu::api::cast<float,float16>(context->XPUHandle(), (float*)aData, (float16*)cData, len);
+			break;
+		case CastType::Float2Int64:
+			ret = baidu::xpu::api::cast<float,int64_t>(context->XPUHandle(), (float*)aData, (int64_t*)cData, len);
+			break;
 		case CastType::Float2Int32:
 			ret = baidu::xpu::api::cast<float,int>(context->XPUHandle(), (float*)aData, (int*)cData, len);
 			break;
-		case CastType::Int322Int8:
+		case CastType::Float2Int16:
+			ret = baidu::xpu::api::cast<float,int16_t>(context->XPUHandle(), (float*)aData, (int16_t*)cData, len);
+			break;
+		case CastType::Float2Int8:
+			ret = baidu::xpu::api::cast<float,int8_t>(context->XPUHandle(), (float*)aData, (int8_t*)cData, len);
+			break;
+		case CastType::Int322Float:
 			ret = baidu::xpu::api::cast<int,float>(context->XPUHandle(), (int*)aData, (float*)cData, len);
+			break;
+		case CastType::Int322Int8:
+			ret = baidu::xpu::api::cast<int,int8_t>(context->XPUHandle(), (int*)aData, (int8_t*)cData, len);
+			break;
+		case CastType::Int322Int16:
+			ret = baidu::xpu::api::cast<int,int16_t>(context->XPUHandle(), (int*)aData, (int16_t*)cData, len);
+			break;
+		case CastType::Int162Float:
+			ret = baidu::xpu::api::cast<int16_t,float>(context->XPUHandle(), (int16_t*)aData, (float*)cData, len);
+			break;
+		case CastType::Int162Int32:
+			ret = baidu::xpu::api::cast<int16_t,int>(context->XPUHandle(), (int16_t*)aData, (int*)cData, len);
+			break;
+		case CastType::Int82Float:
+			ret = baidu::xpu::api::cast<int8_t,float>(context->XPUHandle(), (int8_t*)aData, (float*)cData, len);
+			break;
+		case CastType::Int82Int16:
+			ret = baidu::xpu::api::cast<int8_t,int16_t>(context->XPUHandle(), (int8_t*)aData, (int16_t*)cData, len);
+			break;
+		case CastType::Int82Int32:
+			ret = baidu::xpu::api::cast<int8_t,int>(context->XPUHandle(), (int8_t*)aData, (int*)cData, len);
+			break;
+		case CastType::Int322Int64:
+			ret = baidu::xpu::api::cast<int,int64_t>(context->XPUHandle(), (int*)aData, (int64_t*)cData, len);
+			break;
+		case CastType::Int642Int32:
+			ret = baidu::xpu::api::cast<int64_t,int>(context->XPUHandle(), (int64_t*)aData, (int*)cData, len);
+			break;
+		case CastType::Int642Float:
+			ret = baidu::xpu::api::cast<int64_t,float>(context->XPUHandle(), (int64_t*)aData, (float*)cData, len);
+			break;
+		case CastType::Float162Float:
+			ret = baidu::xpu::api::cast<float16,float>(context->XPUHandle(), (float16*)aData, (float*)cData, len);
 			break;
 		default:
 			IT_TODO_HALT();
