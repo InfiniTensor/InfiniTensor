dfb80bf2af0d4fee52c863364f356181d109a66b
panzezhong <panzezhong@qiyuanlab.com>
2023-08-29 09:53:54 +0800

    Add comments for operators

--- include/operators/all_reduce.h
+++ include/operators/all_reduce.h
@@ -2,17 +2,41 @@
 #include "core/operator.h"
 
 namespace infini {
+/**
+ * @brief The AllReduce operation is performing reductions on data (sum, min,
+ * max, avg, or div) across devices and writing the result in the
+ * receive buffers of every rank. For example, in an allreduce operation between
+ * k ranks and performing a sum, each rank will provide an array Vk of N values,
+ * and receive an identical arrays S of N values, where S[i] =
+ * V0[i]+V1[i]+…+Vk-1[i].
+ *
+ * For more details:
+ * https://docs.nvidia.com/deeplearning/nccl/user-guide/docs/usage/collectives.html#allreduce
+ */
 class AllReduceBaseObj : public OperatorObj {
 
   public:
+    /**
+     * @brief Construct a new AllReduce base object. Should be called by every
+     * child class constructor, but not directly.
+     *
+     * @param graph The computation graph that this operator belongs to.
+     * @param opType The operation type. This param is taken care of by child
+     * classes.
+     * @param input The input tensor from this rank.
+     * @param output The output tensor, same size as input.
+     */
     AllReduceBaseObj(GraphObj *graph, OpType opType, Tensor input,
                      Tensor output);
     OP_CLONE(AllReduceBaseObj);
+
     int numInputs() const override { return 1; }
     int numOutputs() const override { return 1; }
+
     optional<vector<Shape>> inferShape(const TensorVec &inputs) const override {
         return {{inputs[0]->getDims()}};
     };
+
     std::string toString() const override;
 
   private:
