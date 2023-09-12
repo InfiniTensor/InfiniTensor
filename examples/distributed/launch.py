b454cfb4fdd6372bfe236df5f03a1323928d9afe
constroy <constroy.li@gmail.com>
2023-09-12 16:47:58 +0800

    optimize launch.py

--- examples/distributed/launch.py
+++ examples/distributed/launch.py
@@ -19,6 +19,9 @@ def parse_args():
         "--nproc_per_node", type=int, default=1, help="number of processes per node"
     )
     parser.add_argument(
+        "--name", type=str, default="test", help="name of this instance."
+    )
+    parser.add_argument(
         "--model", type=str, required=True, help="path to the ONNX model file."
     )
     parser.add_argument("--batch_size", type=int, default=1, help="batch size.")
@@ -33,6 +36,7 @@ def parse_args():
     return (
         args.num_nodes,
         args.nproc_per_node,
+        args.name,
         args.model,
         args.batch_size,
         args.length,
@@ -40,12 +44,12 @@ def parse_args():
     )
 
 
-def run_stub(stub: OnnxStub, inputs: np.array, n=20):
-    # warm up
+def run_model(model, runtime, inputs: np.array, n=20):
+    stub = OnnxStub(model, runtime)
     next(stub.inputs.items().__iter__())[1].copyin_numpy(inputs)
     stub.tune()
-    for _ in range(10):
-        stub.run()
+    stub.run()
+    # get outputs
     outputs = np.array(next(stub.outputs.items().__iter__())[1].copyout_float())
 
     # bench
@@ -54,18 +58,27 @@ def run_stub(stub: OnnxStub, inputs: np.array, n=20):
     for _ in range(n):
         stub.run()
     end = time.time()
-    outputs = np.array(next(stub.outputs.items().__iter__())[1].copyout_float())
     avg_time = (end - begin) / n
     print(f"average time: {avg_time}")
     return outputs
 
 
+def run_and_compare(name, model, runtime):
+    data = np.load(f"{name}_inputs.npy")
+    results = np.load(f"{name}_results.npy")
+    outputs = run_model(model, runtime, data)
+    print("outputs sum:", outputs.sum())
+    print("max abs diff:", abs(outputs - results).max())
+    print("max rel diff:", abs((outputs - results) / results).max())
+    # assert np.allclose(outputs, results, rtol=1e-3, atol=1e-6)
+
+
 def start_worker(
-    dist_name: str, world_size: int, rank: int, local_rank: int, model: onnx.ModelProto
+    name: str, world_size: int, rank: int, local_rank: int, model: onnx.ModelProto
 ):
-    # print("start worker")
+    dist_name = name + "_dist"
     model = parallel_model(model, world_size, rank)
-    extern_path = f"./dist_model_rank{rank}.pb"
+    extern_path = f"./{dist_name}_rank{rank}.pb"
     if os.path.exists(extern_path):
         os.remove(extern_path)
     convert_model_to_external_data(
@@ -75,7 +88,7 @@ def start_worker(
         size_threshold=1024,
         convert_attribute=False,
     )
-    onnx.save(model, f"dist_model_rank{rank}.onnx")
+    onnx.save(model, f"./{dist_name}_rank{rank}.onnx")
     runtime = backend.CudaRuntime(local_rank)
     # print("init comm")
     runtime.init_comm(
@@ -83,47 +96,48 @@ def start_worker(
         world_size,
         rank,
     )
-    # print("load model")
-    stub = OnnxStub(model, runtime)
-    data = np.load("inputs.npy")
-    # print("run model")
-    outputs = run_stub(stub, data)
-    print("outputs sum:", outputs.sum())
-    results = np.load("results.npy")
-    print("max abs diff:", abs(outputs - results).max())
-    print("max rel diff:", abs((outputs - results) / results).max())
-    # assert np.allclose(outputs, results, rtol=1e-3, atol=1e-6)
+    run_and_compare(name, model, runtime)
 
 
-def run_standard(model, voc_size, bs, len):
-    # generate standard results
+def start_single(name, model):
     runtime = backend.CudaRuntime(0)
-    stub = OnnxStub(model, runtime)
-    # data = np.zeros((bs, len), dtype=np.int32)
+    run_and_compare(name, model, runtime)
+
+
+def gen_standard(name, model, voc_size, bs, len):
+    # generate standard results
     data = np.random.randint(0, voc_size, (bs, len), dtype=np.int32)
-    np.save("inputs", data)
-    outputs = run_stub(stub, data)
-    print("outputs sum:", outputs.sum())
-    np.save("results", outputs)
+    np.save(f"{name}_inputs", data)
+    runtime = backend.CudaRuntime(0)
+    outputs = run_model(model, runtime, data, 1)
+    np.save(f"{name}_results", outputs)
 
 
 def main():
-    nnodes, nproc_per_node, model_path, bs, length, gen_std = parse_args()
+    nnodes, nproc_per_node, name, model_path, bs, length, gen_std = parse_args()
 
     model = onnx.load(model_path)
 
+    # generate standart output
     if gen_std:
-        voc_size = 1000  # a small vocabulary size to fit all LLM.
-        p = mp.Process(target=run_standard, args=(model, voc_size, bs, length))
-        p.start()
-        p.join()
+        print(f"generate standard data for {name}.")
+        # a small vocabulary size to fit all LLM.
+        voc_size = 1000
+        gen_standard(name, model, voc_size, bs, length)
+        return
 
+    # run single process.
+    # use standalone process to isolate cuda.
+    p = mp.Process(target=start_single, args=(name, model))
+    p.start()
+    p.join()
+
+    # run distributed parallel.
     world_size = nnodes * nproc_per_node
-    dist_name = f"dist_{os.getpid()}"
     workers = [
         mp.Process(
             target=start_worker,
-            args=(dist_name, world_size, rank, rank % nproc_per_node, model),
+            args=(name, world_size, rank, rank % nproc_per_node, model),
         )
         for rank in range(world_size)
     ]
