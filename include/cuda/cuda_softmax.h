106e75a4e3819d6f308c9792ae3c630e42935645
xgqdut2016 <kenan_gewei@163.com>
2023-09-21 11:19:09 +0800

    remodify 1D block softmax

--- include/cuda/cuda_softmax.h
+++ include/cuda/cuda_softmax.h
@@ -1,6 +1,6 @@
 #pragma once
 #include "utils/small_array.h"
 namespace infini {
-void softmax_kernel(float *input, float *output, int size, int size_y,
+void softmax_kernel(int num_blocks, float *input, float *output, int size,
                     int dimsize, int stride);
 }
--- src/kernels/cuda/softmax.cc
+++ src/kernels/cuda/softmax.cc
@@ -11,20 +11,19 @@ class SoftmaxCuda : public CudaKernelWithoutConfig {
         auto op = as<SoftmaxObj>(_op);
         auto input = op->getInputs(0)->getRawDataPtr<float *>();
         auto output = op->getOutput(0)->getRawDataPtr<float *>();
-        const auto &inputShape = op->getInputs(0)->getDims(); // input shape
-        int nDims = op->getInputs(0)->getDims().size();
-        IT_ASSERT(nDims <= SMALL_ARRAY_SIZE);
-        int size = 1;
-
-        for (int i = 0; i < nDims; ++i) {
-            size *= inputShape[i];
-        }
+        const auto &inShape = op->getInputs(0)->getDims(); // input shape
         auto dims = op->getInputs(0)->getDims();
+        int nDims = dims.size();
+        int size = 1; // size = i(JKS) + j(KS) + k(S) + s
         int dimsize = dims[op->getAxis()];
-        int size_y = size / dimsize;
         int stride = op->getInputs(0)->getStride().at(op->getAxis());
-        softmax_kernel((float *)input, (float *)output, size, size_y, dimsize,
-                       stride);
+        for (int i = nDims - 1; i >= 0;
+             --i) { // must i = nDims - 1, --i; can't i = 0, i++
+            size *= inShape[i];
+        }
+        int num_blocks = size / dimsize;
+        softmax_kernel(num_blocks, (float *)input, (float *)output, size,
+                       dimsize, stride);
     }
 };
 
