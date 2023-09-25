62be816f53f7e348611ae6247f96ccd5a3127287
PanZezhong1725 <141193946+PanZezhong1725@users.noreply.github.com>
2023-09-25 10:25:54 +0800

    修复split concat当dim=0结果出错的问题 (#138)
    Fix split_concat kernel not supporting dim=0
    
    Co-authored-by: Haojie Wang <haojie0429@gmail.com>

--- src/kernels/cuda/split_concat.cu
+++ src/kernels/cuda/split_concat.cu
@@ -1,30 +1,29 @@
 #include "cuda/cuda_common.h"
 #include "cuda/cuda_split_concat.h"
 
-int getMultiProcessorCount() {
-    int cur_device;
-    checkCudaError(cudaGetDevice(&cur_device));
-
-    struct cudaDeviceProp prop;
-    checkCudaError(cudaGetDeviceProperties(&prop, cur_device));
-    return prop.multiProcessorCount;
-}
-
 __host__ __device__ int
 elementIdx2ComposedIdx(int elementIndex, int dimBgNo, int dimSize, int dim,
                        int nDim, ComposedTensorMetadata wholeMeta) {
     int offset = 0;
 
+    // COMP(x0,...,xk,...,xn-1) = ELMT[xk / d](x0,...,xk % d,...xn-1)
+    // where k=dim, n=ndim, d=dimSize is the splited length of
+    // dimension dim
 #pragma unroll
+    // Interate through n-1 to 1
     for (int i = nDim - 1; i >= 1; --i) {
         int size = (i == dim) ? dimSize : wholeMeta.dimSize[i];
         int p = elementIndex % size;
+        // dimBgNo move the pointer to correct location in composed data
+        // corresponding to current element, with repect to the splitted
+        // dimension dim
         int oP = (i == dim) ? (p + dimBgNo) : p;
         elementIndex = (elementIndex - p) / size;
         offset += oP * wholeMeta.stride[i];
     }
-
-    return offset + elementIndex * wholeMeta.stride[0];
+    // Deal with i = 0
+    int oP = (dim == 0) ? (elementIndex + dimBgNo) : elementIndex;
+    return offset + oP * wholeMeta.stride[0];
 }
 
 __global__ void _split_concat_kernel(ElementTensorMetadata elemMeta,
@@ -38,31 +37,29 @@ __global__ void _split_concat_kernel(ElementTensorMetadata elemMeta,
     auto dimBgNo = elemMeta.dimBgNo[blockIdx.y];
     auto dimSize = elemMeta.dimSize[blockIdx.y];
     float *elemData = elemMeta.data[blockIdx.y];
-    int stride = gridDim.x * blockDim.x;
 
-    while (tid < nElements) {
-        int Offset =
-            elementIdx2ComposedIdx(tid, dimBgNo, dimSize, dim, nDims, compMeta);
-        // copy data from input to output
-        // for split:input is composed tensor;for concat:input is element
-        // tensors.
-        if (isSplit)
-            elemData[tid] = compMeta.data[Offset];
-        else
-            compMeta.data[Offset] = elemData[tid];
-        tid += stride;
-    }
+    int Offset =
+        elementIdx2ComposedIdx(tid, dimBgNo, dimSize, dim, nDims, compMeta);
+    // copy data from input to output
+    // for split:input is composed tensor;for concat:input is element
+    // tensors.
+    if (isSplit)
+        elemData[tid] = compMeta.data[Offset];
+    else
+        compMeta.data[Offset] = elemData[tid];
 }
 
 namespace infini {
 
+// TODO: when dim=0, the operation can be executed in-place 
 void split_concat_kernel(const ElementTensorMetadata &eleMeta,
                          const ComposedTensorMetadata &compMeta, int dim,
                          int batchSize, int nDims, bool isSplit) {
     dim3 blockSize = dim3(32 * 16);
-
-    //  y dim is number of tensors.
-    dim3 gridSize(getMultiProcessorCount(), batchSize);
+    // gridsize =n_elements / blockSize
+    int gridDimX = (eleMeta.nElements[0] - 1) / (32 * 16) + 1;
+    // each y is a split among the batch
+    dim3 gridSize(gridDimX, batchSize);
 
     _split_concat_kernel<<<gridSize, blockSize>>>(eleMeta, compMeta, dim, nDims,
                                                   isSplit);
