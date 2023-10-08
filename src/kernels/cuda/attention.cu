3629881dfa58c44077b3ea9f8fe383cadf450ec8
xgqdut2016 <kenan_gewei@163.com>
2023-10-08 15:15:39 +0800

    blockreduce matrix times

--- src/kernels/cuda/attention.cu
+++ src/kernels/cuda/attention.cu
@@ -1,5 +1,5 @@
 #include "cuda/cuda_common.h"
-
+#include <cub/block/block_reduce.cuh>
 #define max_function(a, b) ((a) > (b) ? (a) : (b))
 
 template <int BLOCK_DIM_x>
@@ -19,38 +19,31 @@ __launch_bounds__(BLOCK_DIM_x) __global__
 
     int extra = d % BLOCK_DIM_x;
     int step = (d - extra) / BLOCK_DIM_x;
-    __shared__ float shareQ_times_K[BLOCK_DIM_x];
-
+    out[threadIdx.x] = 0.0f;
+    __shared__ float sum_s;
     for (int phn = 0; phn < N; phn++) {
-        shareQ_times_K[threadIdx.x] = 0.0f;
-        float sum_s = 0.0f;
+        float sum_partial = 0.0f;
+
         if (threadIdx.x < extra) {
             for (int ind = threadIdx.x * (step + 1);
                  ind < (threadIdx.x + 1) * (step + 1); ind++) {
-                shareQ_times_K[threadIdx.x] +=
-                    inputQ[i * d + ind] * inputK[phn * d + ind];
+                sum_partial += inputQ[i * d + ind] * inputK[phn * d + ind];
             }
         } else {
             for (int ind = extra * (step + 1) + (threadIdx.x - extra) * step;
                  ind < extra * (step + 1) + (threadIdx.x - extra + 1) * step;
                  ind++) {
-                shareQ_times_K[threadIdx.x] +=
-                    inputQ[i * d + ind] * inputK[phn * d + ind];
+                sum_partial += inputQ[i * d + ind] * inputK[phn * d + ind];
             }
         }
+        typedef cub::BlockReduce<float, BLOCK_DIM_x> BlockReduce;
+        __shared__ typename BlockReduce::TempStorage temp_storage;
+        float block_sum =
+            BlockReduce(temp_storage).Reduce(sum_partial, cub::Sum());
 
+        if (threadIdx.x == 0)
+            sum_s = block_sum;
         __syncthreads();
-        for (int strip = BLOCK_DIM_x / 8; strip > 0; strip = strip / 8) {
-            if (threadIdx.x < strip) {
-                for (int id = 1; id < 8; id++) {
-                    shareQ_times_K[threadIdx.x] +=
-                        shareQ_times_K[threadIdx.x + id * strip];
-                }
-            }
-            __syncthreads();
-        }
-        sum_s = shareQ_times_K[0] + shareQ_times_K[1];
-        //__syncthreads();
 
         if (new_max > sum_s) {
             new_sum = new_sum + __expf(sum_s - new_max);
@@ -59,25 +52,14 @@ __launch_bounds__(BLOCK_DIM_x) __global__
             new_max = sum_s;
         }
 
-        //__syncthreads();
-
         sum_s = __expf(sum_s - new_max);
 
-        //__syncthreads();
-
-        if (phn == 0) {
-            out[threadIdx.x] = sum_s * inputV[phn * d + phd];
-
-        } else {
-            out[threadIdx.x] = __expf(old_max - new_max) * out[threadIdx.x] +
-                               sum_s * inputV[phn * d + phd];
-        }
+        out[threadIdx.x] = __expf(old_max - new_max) * out[threadIdx.x] +
+                           sum_s * inputV[phn * d + phd];
 
         old_max = new_max;
-
-        //__syncthreads();
     }
-    //__syncthreads();
+
     if (phd < d)
         output[i * d + phd] = out[threadIdx.x] * __fdividef(1.0F, new_sum);
 }
@@ -87,19 +69,47 @@ void attentionKernel(const float *inputQ, const float *inputK,
 
     int num_block_y = N;
 
-    if (d > 128) {
+    if (d > 512) {
         int BLOCK_DIM_x = 1024;
         int num_block_x = (d + BLOCK_DIM_x - 1) / BLOCK_DIM_x;
         dim3 block_dim(BLOCK_DIM_x, 1, 1);
         dim3 grid_dim(num_block_x, num_block_y, 1);
         _attentionKernel<1024>
             <<<grid_dim, block_dim>>>(inputQ, inputK, inputV, N, d, output);
-    } else if (d > 16) {
+    } else if (d > 256) {
+        int BLOCK_DIM_x = 512;
+        int num_block_x = (d + BLOCK_DIM_x - 1) / BLOCK_DIM_x;
+        dim3 block_dim(BLOCK_DIM_x, 1, 1);
+        dim3 grid_dim(num_block_x, num_block_y, 1);
+        _attentionKernel<512>
+            <<<grid_dim, block_dim>>>(inputQ, inputK, inputV, N, d, output);
+    } else if (d > 128) {
+        int BLOCK_DIM_x = 256;
+        int num_block_x = (d + BLOCK_DIM_x - 1) / BLOCK_DIM_x;
+        dim3 block_dim(BLOCK_DIM_x, 1, 1);
+        dim3 grid_dim(num_block_x, num_block_y, 1);
+        _attentionKernel<256>
+            <<<grid_dim, block_dim>>>(inputQ, inputK, inputV, N, d, output);
+    } else if (d > 64) {
         int BLOCK_DIM_x = 128;
         int num_block_x = (d + BLOCK_DIM_x - 1) / BLOCK_DIM_x;
         dim3 block_dim(BLOCK_DIM_x, 1, 1);
         dim3 grid_dim(num_block_x, num_block_y, 1);
         _attentionKernel<128>
+            <<<grid_dim, block_dim>>>(inputQ, inputK, inputV, N, d, output);
+    } else if (d > 32) {
+        int BLOCK_DIM_x = 64;
+        int num_block_x = (d + BLOCK_DIM_x - 1) / BLOCK_DIM_x;
+        dim3 block_dim(BLOCK_DIM_x, 1, 1);
+        dim3 grid_dim(num_block_x, num_block_y, 1);
+        _attentionKernel<512>
+            <<<grid_dim, block_dim>>>(inputQ, inputK, inputV, N, d, output);
+    } else if (d > 16) {
+        int BLOCK_DIM_x = 32;
+        int num_block_x = (d + BLOCK_DIM_x - 1) / BLOCK_DIM_x;
+        dim3 block_dim(BLOCK_DIM_x, 1, 1);
+        dim3 grid_dim(num_block_x, num_block_y, 1);
+        _attentionKernel<32>
             <<<grid_dim, block_dim>>>(inputQ, inputK, inputV, N, d, output);
     } else {
         int BLOCK_DIM_x = 16;
