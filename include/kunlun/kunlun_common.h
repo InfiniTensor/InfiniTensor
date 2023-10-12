89e655a7455b8e74b01d5800b3e48dd31def1208
wanghailu0717 <wanghailuTsinghua@outlook.com>
2023-10-12 15:06:37 +0800

    fix format

--- include/kunlun/kunlun_common.h
+++ include/kunlun/kunlun_common.h
@@ -3,18 +3,18 @@
 #include "xpu/runtime_ex.h"
 #include "xpu/xdnn.h"
 
-#define checkKUNLUNError(call)                                             \
-  {                                                                        \
-    auto err = call;                                                       \
-    if (XPU_SUCCESS != err) {                                              \
-      fprintf(stderr, "KUNLUN error in %s:%i : %s.\n", __FILE__, __LINE__, \
-              xpu_strerror(err));                                          \
-      exit(EXIT_FAILURE);                                                  \
-    }                                                                      \
-  }
+#define checkKUNLUNError(call)                                                 \
+    {                                                                          \
+        auto err = call;                                                       \
+        if (XPU_SUCCESS != err) {                                              \
+            fprintf(stderr, "KUNLUN error in %s:%i : %s.\n", __FILE__,         \
+                    __LINE__, xpu_strerror(err));                              \
+            exit(EXIT_FAILURE);                                                \
+        }                                                                      \
+    }
 
 namespace infini {
 
 using KUNLUNPtr = void *;
 
-}  // namespace infini
+} // namespace infini
