8b2e3b8e19e14e1844c93f5fd30fe23fb8e5823d
xgqdut2016 <kenan_gewei@163.com>
2023-12-08 16:57:49 +0800

    add where fp16

--- test/kernels/cuda/test_cuda_where.cc
+++ test/kernels/cuda/test_cuda_where.cc
@@ -8,11 +8,11 @@
 
 namespace infini {
 
-void test_where(const Shape &inputXShape, const vector<float> &inputXData,
-                const Shape &inputYShape, const vector<float> &inputYData,
-                const Shape &conditionShape,
-                const vector<uint8_t> &conditionData,
-                const vector<float> &ExpectData) {
+void test_whereFp32(const Shape &inputXShape, const vector<float> &inputXData,
+                    const Shape &inputYShape, const vector<float> &inputYData,
+                    const Shape &conditionShape,
+                    const vector<uint8_t> &conditionData,
+                    const vector<float> &ExpectData) {
     Runtime runtime = NativeCpuRuntimeObj::getInstance();
     Graph gCpu = make_ref<GraphObj>(runtime);
     auto condition = gCpu->addTensor(conditionShape, DataType::UInt8);
@@ -43,22 +43,62 @@ void test_where(const Shape &inputXShape, const vector<float> &inputXData,
     oCpu->printData();                              //->printData
     EXPECT_TRUE(oCpu->equalData(ExpectData));
 }
+void test_whereFp16(
+    const Shape &inputXShape,
+    const std::function<void(void *, size_t, DataType)> &generatorX,
+    const Shape &inputYShape,
+    const std::function<void(void *, size_t, DataType)> &generatorY,
+    const Shape &conditionShape, const vector<uint8_t> &conditionData,
+    const vector<float> &ExpectData) {
+    Runtime runtime = NativeCpuRuntimeObj::getInstance();
+    Graph gCpu = make_ref<GraphObj>(runtime);
 
-TEST(CUDA_Where, run) {
-    test_where(
+    auto inputX = gCpu->addTensor(inputXShape, DataType::Float16);
+    auto inputY = gCpu->addTensor(inputYShape, DataType::Float16);
+    auto condition = gCpu->addTensor(conditionShape, DataType::UInt8);
+    gCpu->dataMalloc();
+
+    inputX->setData(generatorX);
+    inputY->setData(generatorY);
+    condition->copyin(conditionData); //
+
+    auto cudaRuntime = make_ref<CudaRuntimeObj>();
+    Graph gCuda = make_ref<GraphObj>(cudaRuntime);
+
+    auto inputXGpu = gCuda->cloneTensor(inputX);
+    auto inputYGpu = gCuda->cloneTensor(inputY);
+    auto conditionGpu = gCuda->cloneTensor(condition);
+
+    auto op = gCuda->addOp<WhereObj>(inputXGpu, inputYGpu, conditionGpu,
+                                     nullptr); // WhereObj
+    gCuda->dataMalloc();
+
+    inputXGpu->setData(generatorX);
+    inputYGpu->setData(generatorY);
+    conditionGpu->copyin(conditionData);
+    cudaRuntime->run(gCuda);
+
+    auto oCpu = gCpu->cloneTensor(op->getOutput()); // move Data from gpu to cpu
+    oCpu->printData();                              //->printData
+    EXPECT_TRUE(oCpu->equalData(ExpectData));
+}
+
+TEST(CUDA_WhereFp32, run) {
+    test_whereFp32(
         Shape{2, 2, 3, 1}, vector<float>{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11},
         Shape{2, 2, 3, 1}, vector<float>{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
         Shape{2, 2, 3, 1}, vector<uint8_t>{0, 1, 1, 0, 0, 0, 1, 1, 0, 1, 1, 1},
         vector<float>{0., 1., 2., 0., 0., 0., 6., 7., 0., 9., 10., 11.});
 
-    test_where(Shape{2, 1, 1, 3},                                  // inputx
-               vector<float>{0, 1, 2, 3, 4, 5}, Shape{1, 2, 1, 1}, // inputy
-               vector<float>{1, 1}, Shape{2, 1, 3, 1},             // condition
-               vector<uint8_t>{0, 1, 1, 0, 0, 0},
-               vector<float>{1., 1., 1., 0., 1., 2., 0., 1., 2., 1., 1., 1.,
-                             0., 1., 2., 0., 1., 2., 1., 1., 1., 1., 1., 1.,
-                             1., 1., 1., 1., 1., 1., 1., 1., 1., 1., 1., 1.});
-    test_where(
+    test_whereFp32(Shape{2, 1, 1, 3},                                  // inputx
+                   vector<float>{0, 1, 2, 3, 4, 5}, Shape{1, 2, 1, 1}, // inputy
+                   vector<float>{1, 1}, Shape{2, 1, 3, 1}, // condition
+                   vector<uint8_t>{0, 1, 1, 0, 0, 0},
+                   vector<float>{1., 1., 1., 0., 1., 2., 0., 1., 2.,
+                                 1., 1., 1., 0., 1., 2., 0., 1., 2.,
+                                 1., 1., 1., 1., 1., 1., 1., 1., 1.,
+                                 1., 1., 1., 1., 1., 1., 1., 1., 1.});
+    test_whereFp32(
         Shape{
             3,
         },
@@ -68,7 +108,7 @@ TEST(CUDA_Where, run) {
         vector<float>{0., 0., 0., 0., 1., 2., 0., 1., 2., 3., 3., 3.,
                       0., 1., 2., 0., 1., 2., 0., 0., 0., 1., 1., 1.,
                       2., 2., 2., 3., 3., 3., 4., 4., 4., 5., 5., 5.});
-    test_where(
+    test_whereFp32(
         Shape{
             3,
         },
@@ -79,6 +119,30 @@ TEST(CUDA_Where, run) {
         vector<float>{0., 0., 0., 0., 1., 2., 0., 1., 2., 3., 3., 3.,
                       0., 1., 2., 0., 1., 2., 0., 0., 0., 1., 1., 1.,
                       2., 2., 2., 3., 3., 3., 4., 4., 4., 5., 5., 5.});
+
+} // python output
+TEST(CUDA_WhereFp16, run) {
+    test_whereFp16(
+        Shape{
+            3,
+        },
+        ValGenerator<1>(),                                    // inputX
+        Shape{2, 3, 1}, ValGenerator<2>(),                    // inputY
+        Shape{2, 1, 3, 1}, vector<uint8_t>{0, 1, 1, 0, 0, 0}, // condition
+        vector<float>{2., 2., 2., 1., 1., 1., 1., 1., 1., 2., 2., 2.,
+                      1., 1., 1., 1., 1., 1., 2., 2., 2., 2., 2., 2.,
+                      2., 2., 2., 2., 2., 2., 2., 2., 2., 2., 2., 2.});
+    test_whereFp16(
+        Shape{
+            3,
+        },
+        ValGenerator<1>(),                 // inputX
+        Shape{2, 3, 1}, ValGenerator<2>(), // inputY
+        Shape{2, 1, 3, 1},
+        vector<uint8_t>{false, true, true, false, false, false}, // condition
+        vector<float>{2., 2., 2., 1., 1., 1., 1., 1., 1., 2., 2., 2.,
+                      1., 1., 1., 1., 1., 1., 2., 2., 2., 2., 2., 2.,
+                      2., 2., 2., 2., 2., 2., 2., 2., 2., 2., 2., 2.});
 
 } // python output
 
