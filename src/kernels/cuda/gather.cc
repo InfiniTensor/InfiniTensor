2af4c1276b28a12dc7ec84445f91360cca442f88
OdinaryWord <sx-hz@163.com>
2023-12-14 13:28:41 +0800

    feat:support int8 for gather

--- src/kernels/cuda/gather.cc
+++ src/kernels/cuda/gather.cc
@@ -21,8 +21,12 @@ class GatherCuda : public CudaKernelWithoutConfig {
         if (op->getDType() == DataType::Float32) {
             gather_kernel<float>((float *)inputData, (float *)outputData,
                                  metaData, op->getOutput()->size());
-        } else if (op->getDType() == DataType::Float32) {
+        } else if (op->getDType() == DataType::Float16) {
             gather_kernel<half>((half *)inputData, (half *)outputData, metaData,
+                                op->getOutput()->size());
+        }
+        else if (op->getDType() == DataType::Int8) {
+            gather_kernel<int8_t>((int8_t *)inputData, (int8_t *)outputData, metaData,
                                 op->getOutput()->size());
         }
     }
