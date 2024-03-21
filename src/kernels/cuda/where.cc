c4d0673ae21bde920371ffc6d7216924688b26b3
xgqdut2016 <kenan_gewei@163.com>
2024-03-21 11:21:47 +0800

    modified where index

--- src/kernels/cuda/where.cc
+++ src/kernels/cuda/where.cc
@@ -3,7 +3,6 @@
 #include "cuda/cuda_runtime.h"
 #include "cuda/cuda_utility.h"
 #include "cuda/cuda_where.h"
-#include "utils/broadcast_shape.h"
 
 namespace infini {
 
@@ -16,31 +15,31 @@ class WhereCuda : public CudaKernelWithoutConfig {
         void *const inputYData = (op->getInputs(1)->getRawDataPtr<void *>());
         void *const conditionData = (op->getInputs(2)->getRawDataPtr<void *>());
         void *const outputData = (op->getOutput()->getRawDataPtr<void *>());
-        const auto &opInputXShape = op->getInputs(0)->getDims();
-        const auto &opInputYShape = op->getInputs(1)->getDims();
-        const auto &opConditionShape = op->getInputs(2)->getDims();
-        const auto &opOutputShape = op->getOutput()->getDims();
 
-        const int xSize = op->getInputs(0)->getRank();
-        const int ySize = op->getInputs(1)->getRank();
-        const int cSize = op->getInputs(2)->getRank();
+        auto a_dim = op->getInputs(0)->getDims();
+        auto b_dim = op->getInputs(1)->getDims();
+        auto c_dim = op->getInputs(2)->getDims();
+        auto d_dim = op->getOutput()->getDims();
 
-        int nDims = op->getOutput()->getDims().size();
-        IT_ASSERT(nDims <= SMALL_ARRAY_SIZE);
-        int outputsize = 1;
-        SmallArray inputXShape, inputYShape, conditionShape, outputShape;
-        for (int i = nDims - 1; i >= 0; --i) {
-            outputShape.data[i] = opOutputShape[i];
-            outputsize *= outputShape.data[i];
-        }
-        broadcastShape(opInputXShape, inputXShape, nDims, xSize);
-        broadcastShape(opInputYShape, inputYShape, nDims, ySize);
-        broadcastShape(opConditionShape, conditionShape, nDims, cSize);
+        if (a_dim.size() > 4 || b_dim.size() > 4 || c_dim.size() > 4 ||
+            d_dim.size() > 4)
+            IT_TODO_HALT();
+
+        int a[4] = {1, 1, 1, 1};
+        int b[4] = {1, 1, 1, 1};
+        int c[4] = {1, 1, 1, 1};
+        int d[4] = {1, 1, 1, 1};
+
+        std::copy(a_dim.begin(), a_dim.end(), a + (4 - a_dim.size()));
+        std::copy(b_dim.begin(), b_dim.end(), b + (4 - b_dim.size()));
+        std::copy(c_dim.begin(), c_dim.end(), c + (4 - c_dim.size()));
+        std::copy(d_dim.begin(), d_dim.end(), d + (4 - d_dim.size()));
+
         const int dTypeIndex = op->getDType().getIndex();
         whereKernel(dTypeIndex, inputXData, inputYData,
-                    (uint8_t *)conditionData, outputData, nDims, outputsize,
-                    inputXShape, inputYShape, conditionShape, outputShape,
-                    xSize, ySize, cSize);
+                    (uint8_t *)conditionData, outputData, a[0], a[1], a[2],
+                    a[3], b[0], b[1], b[2], b[3], c[0], c[1], c[2], c[3], d[0],
+                    d[1], d[2], d[3]);
     }
 };
 
--- src/kernels/cuda/where.cu
+++ src/kernels/cuda/where.cu
@@ -1,50 +1,51 @@
 #include "cuda/cuda_common.h"
 #include "cuda/cuda_utility.h"
-#include "utils/small_array.h"
 
-__device__ int inferIndex(infini::SmallArray inputShape,
-                          infini::SmallArray outputShape, int nDims, int size,
-                          int outputIdx) {
-    int inputIdx = 0;
-    int tempInput = 1;
-    int tempOutput = 1;
-    for (int i = nDims - 1; i >= nDims - size; --i) {
-        tempOutput = outputIdx % outputShape.data[i];
-        if (inputShape.data[i] != 1) {
-            inputIdx += tempInput * tempOutput;
-        }
-        tempInput *= inputShape.data[i];
-        outputIdx /= outputShape.data[i];
-    }
-    return inputIdx;
-}
 template <typename T>
 __global__ void
 _whereKernel(void *inputX, void *inputY, const uint8_t *condition, void *output,
-             int nDims, int outputsize, infini::SmallArray inputXShape,
-             infini::SmallArray inputYShape, infini::SmallArray conditionShape,
-             infini::SmallArray outputShape, int xSize, int ySize, int cSize) {
+             int a0, int a1, int a2, int a3, int b0, int b1, int b2, int b3,
+             int c0, int c1, int c2, int c3, int d0, int d1, int d2, int d3) {
 
-    int outputIdx = blockIdx.x * blockDim.x + threadIdx.x;
-    if (outputIdx < outputsize) {
-        int conditionIdx =
-            inferIndex(conditionShape, outputShape, nDims, cSize, outputIdx);
-        int inputXIdx =
-            inferIndex(inputXShape, outputShape, nDims, xSize, outputIdx);
+    int i = blockIdx.x * blockDim.x + threadIdx.x;
+    int outputsize = d0 * d1 * d2 * d3;
+    if (i < outputsize) {
 
-        int inputYIdx =
-            inferIndex(inputYShape, outputShape, nDims, ySize, outputIdx);
+        int d0_index = i / (d1 * d2 * d3);
+        int d1_index = (i % (d1 * d2 * d3)) / (d2 * d3);
+        int d2_index = ((i % (d1 * d2 * d3)) % (d2 * d3)) / d3;
+        int d3_index = ((i % (d1 * d2 * d3)) % (d2 * d3)) % d3;
 
-        ((T *)output)[outputIdx] = condition[conditionIdx]
-                                       ? ((T *)inputX)[inputXIdx]
-                                       : ((T *)inputY)[inputYIdx];
+        int a0_index = d0_index % a0;
+        int a1_index = d1_index % a1;
+        int a2_index = d2_index % a2;
+        int a3_index = d3_index % a3;
+
+        int b0_index = d0_index % b0;
+        int b1_index = d1_index % b1;
+        int b2_index = d2_index % b2;
+        int b3_index = d3_index % b3;
+
+        int c0_index = d0_index % c0;
+        int c1_index = d1_index % c1;
+        int c2_index = d2_index % c2;
+        int c3_index = d3_index % c3;
+
+        int inputXIdx = a0_index * a1 * a2 * a3 + a1_index * a2 * a3 +
+                        a2_index * a3 + a3_index;
+        int inputYIdx = b0_index * b1 * b2 * b3 + b1_index * b2 * b3 +
+                        b2_index * b3 + b3_index;
+        int conditionIdx = c0_index * c1 * c2 * c3 + c1_index * c2 * c3 +
+                           c2_index * c3 + c3_index;
+        ((T *)output)[i] = condition[conditionIdx] ? ((T *)inputX)[inputXIdx]
+                                                   : ((T *)inputY)[inputYIdx];
     }
 }
 #define CASE(T)                                                                \
     _whereKernel<DT_CUDA<T>::t>                                                \
         <<<gridsize, blocksize, 0, CUDAStream::stream>>>(                      \
-            inputX, inputY, condition, output, nDims, outputsize, inputXShape, \
-            inputYShape, conditionShape, outputShape, xSize, ySize, cSize);
+            inputX, inputY, condition, output, a0, a1, a2, a3, b0, b1, b2, b3, \
+            c0, c1, c2, c3, d0, d1, d2, d3);
 
 #define SWITCH_DTYPE(DTYPE)                                                    \
     switch (DTYPE) {                                                           \
@@ -90,11 +91,11 @@ _whereKernel(void *inputX, void *inputY, const uint8_t *condition, void *output,
 namespace infini {
 
 void whereKernel(int dTypeIndex, void *inputX, void *inputY,
-                 const uint8_t *condition, void *output, int nDims,
-                 int outputsize, SmallArray inputXShape, SmallArray inputYShape,
-                 SmallArray conditionShape, SmallArray outputShape, int xSize,
-                 int ySize, int cSize) {
+                 const uint8_t *condition, void *output, int a0, int a1, int a2,
+                 int a3, int b0, int b1, int b2, int b3, int c0, int c1, int c2,
+                 int c3, int d0, int d1, int d2, int d3) {
     int blocksize;
+    int outputsize = d0 * d1 * d2 * d3;
     if (outputsize > 511) {
         blocksize = 1024;
     } else if (outputsize > 255) {
