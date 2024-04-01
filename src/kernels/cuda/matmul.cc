7f6aec6c17cc64a1129301519c73abe2d3de2f69
PanZezhong1725 <141193946+PanZezhong1725@users.noreply.github.com>
2024-04-01 14:04:28 +0800

    针对bert和gpt2模型分布式推理的优化 (#221)
    * fix(dist): 改善分布式脚本，只打印绝对误差
    
    * feat(dist): 增加可导出onnx的pytorch运行脚本
    
    * feat(front): 增加对Y值为-inf的where算子的图优化
    
    * feat(kernel): 对b为常数的pow和div算子进行特判优化
    
    * fix(front): 消除前端对global output形状信息的依赖，分布式脚本删除不必要的shape infer
    
    * feat(kernel): 针对matmul中bias为行向量时的expand操作的特化优化
    
    * fix(kernel): 删除div pow const中不必要的同步
    
    * Update expand.cu
    
    * fix: fix comments
    
    ---------
    
    Co-authored-by: Haojie Wang <haojie0429@gmail.com>
    Co-authored-by: Derui Yang <ydrml@hotmail.com>

--- src/kernels/cuda/matmul.cc
+++ src/kernels/cuda/matmul.cc
@@ -102,9 +102,19 @@ class matmulCublas : public Kernel {
                     inputShape.data[i] = inC->getDims()[i - offset];
             }
             const int dType = dataType.getIndex();
-            expandKernel(dType, inC->getRawDataPtr<void *>(),
-                         out->getRawDataPtr<void *>(), nDims, outputsize,
-                         inputShape, outputShape);
+
+            // Bias in linear layer is row vector of (1,n), n is the number of
+            // features. If row vector and n % 32 == 0, use optimized kernel.
+            if (inC->getRank() == 1 && inC->getDims()[0] % 32 == 0) {
+                expandRowKernel(dType, inC->getRawDataPtr<void *>(),
+                                out->getRawDataPtr<void *>(),
+                                out->size() / inC->getDims()[0],
+                                inC->getDims()[0]);
+            } else {
+                expandKernel(dType, inC->getRawDataPtr<void *>(),
+                             out->getRawDataPtr<void *>(), nDims, outputsize,
+                             inputShape, outputShape);
+            }
         }
         // TODO:use compute type
         cublasStatus_t stat;
