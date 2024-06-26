0afcd1f99bc6fead0763410222bbad91e2c570d9
sunjqa <sunjqa1@163.com>
2024-06-26 21:32:45 +0800

    Format code using clang-format

--- pyinfinitensor/tests/test_onnx.py
+++ pyinfinitensor/tests/test_onnx.py
@@ -597,7 +597,7 @@ class TestDynamicTensor(unittest.TestCase):
     def test_dynamic_tensor(self):
         filename = r"resnet18-v2-7.onnx"
         current_path = os.getcwd()
-        model_file = ""
+        model_file = "./resnet18-v2-7.onnx"
         for root, dirs, files in os.walk(current_path):
             if filename in files:
                 model_file = os.path.join(root, filename)
