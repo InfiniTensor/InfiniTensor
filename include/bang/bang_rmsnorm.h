c00065be8f8b67622dcc592d82b80fb23c565590
xgqdut2016 <kenan_gewei@163.com>
2024-08-02 10:45:01 +0800

    modified format

--- include/bang/bang_rmsnorm.h
+++ include/bang/bang_rmsnorm.h
@@ -4,4 +4,3 @@ namespace infini {
 void rmsNormKernel(cnnlHandle_t handle, float *mlu_destination, float *mlu_src,
                    float *mlu_weight, int othersize, int dimsize, float eps);
 }; // namespace infini
-
