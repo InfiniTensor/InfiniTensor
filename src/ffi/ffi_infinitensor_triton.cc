c8ec21329c1d4d1befef6c5b40844bcfeeb9f5ae
JYMiracle305 <604951424@qq.com>
2024-11-29 17:45:33 +0800

    以where算子为例进行对接

--- src/ffi/ffi_infinitensor_triton.cc
+++ src/ffi/ffi_infinitensor_triton.cc
@@ -6,6 +6,51 @@
 #include "ffi/ffi_infinitensor_triton.h"
 namespace py = pybind11;
 
+namespace infini {
+
+void whereKernel_py(const float *inputX, const float *inputY,
+                 const uint8_t *condition, float *output, int nDims,
+                 int outputsize, SmallArray inputXShape, SmallArray inputYShape,
+                 SmallArray conditionShape, SmallArray outputShape, int xSize,
+                 int ySize, int cSize) {
+
+    py::scoped_interpreter guard{};
+
+    char cwd[1024];
+    if (getcwd(cwd, sizeof(cwd)) != nullptr) {
+        // 构建 Python 模块的路径
+        std::string op_module_path = std::string(cwd) + "/python/tritonOp";
+        std::cout << "op_module_path: " << op_module_path << std::endl;
+
+        const char* home_dir = std::getenv("HOME");
+        std::string python_module_path;
+        if (home_dir != nullptr) {
+            python_module_path = std::string(home_dir) + "/.local/lib/python3.10/site-packages";
+            std::cout << "python_module_path: " << python_module_path << std::endl;
+        } else {
+            std::cerr << "HOME environment variable is not set." << std::endl;
+        }
+
+        // 导入 sys 模块并添加模块路径
+        py::module_ sys = py::module_::import("sys");
+        sys.attr("path").attr("insert")(0, py::str(op_module_path));
+        sys.attr("path").attr("insert")(0, py::str(python_module_path));
+
+        // 导入 vector_add 模块
+        py::module_ main_module = py::module_::import("where_op");
+        py::object whereKernel = main_module.attr("whereKernel");
+
+        // 调用 Python 的 vector_add 函数
+        whereKernel(inputX, inputY, condition, output, nDims, outputsize,
+            inputXShape, inputYShape, conditionShape, outputShape, xSize, ySize, cSize);
+
+        std::cout << "Result: " << *output << std::endl;
+    } else {
+        std::cerr << "Error getting current working directory: " << strerror(errno) << std::endl;
+    }
+}
+
+
 void print_test_add() {
     // 创建一个 scoped_interpreter 对象，构造时初始化 Python 解释器，析构时清理
     py::scoped_interpreter guard{};
@@ -57,4 +102,6 @@ void print_test_add() {
     } else {
         std::cerr << "Error getting current working directory: " << strerror(errno) << std::endl;
     }
+}
+
 }
--- src/ffi/main_test.cc
+++ src/ffi/main_test.cc
@@ -1,7 +1,10 @@
 #include <iostream>
 #include "ffi/ffi_infinitensor_triton.h"
 
+using namespace infini;
+
 int main() {
     print_test_add();
     return 0;
 }
+
