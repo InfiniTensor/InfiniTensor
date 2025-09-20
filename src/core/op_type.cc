c14b74fadcd91f108fa433c982bab7e205134b99
lzyy2024 <l1z2asd@foxmail.com>
2025-09-20 14:30:09 +0000

    clang-format

--- src/core/op_type.cc
+++ src/core/op_type.cc
@@ -244,9 +244,8 @@ bool OpType::isUnary() const {
 
 bool OpType::isBinary() const {
     static const std::unordered_set<decltype(type)> set{
-        Add,        And,       BitwiseAnd,
-        BitwiseNot, BitwiseOr, BitwiseXor,   Div,           Mod,
-        Mul,        Or,        Pow,          Sub,           Xor,
+        Add, And, BitwiseAnd, BitwiseNot, BitwiseOr, BitwiseXor, Div,
+        Mod, Mul, Or,         Pow,        Sub,       Xor,
     };
 
     return set.find(type) != set.end() || isCompair();
